f5c169db9fe55ac754df76421542ec28c4e5ddcf
Lahiru Sandaruwan <lahirus@wso2.com>
2013-07-07 17:47:33 +0530

    Refactoring org.wso2.carbon.cartridge.mgt.ui to org.apache.stratos.cartridge.mgt.ui

---
+++ components/org.apache.stratos.cartridge.mgt.ui/src/main/java/org/apache/stratos/cartridge/mgt/ui/Payload.java
@@ -0,0 +1,14 @@
+package org.apache.stratos.cartridge.mgt.ui;
+
+public class Payload {
+	private Repository repository;
+
+	public Repository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(Repository repository) {
+		this.repository = repository;
+	}
+
+}
---
+++ components/org.apache.stratos.cartridge.mgt.ui/src/main/java/org/apache/stratos/cartridge/mgt/ui/Repository.java
@@ -0,0 +1,14 @@
+package org.apache.stratos.cartridge.mgt.ui;
+
+public class Repository {
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
