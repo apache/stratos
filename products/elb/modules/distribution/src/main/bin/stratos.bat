40184888fba46e0cd864178f73eef667248aff18
Imesh Gunaratne <imesh@wso2.com>
2013-07-21 15:46:16 +0530

    Added stratos controller to products and updated licenses and artifact names
    Signed-off-by: Lakmal Warusawithana <lakmal@wso2.com>

--- products/elb/modules/distribution/src/main/bin/stratos.bat
+++ products/elb/modules/distribution/src/main/bin/stratos.bat
@@ -1,27 +1,26 @@
 @echo off
 
-REM ---------------------------------------------------------------------------
-REM        Copyright 2005-2009 WSO2, Inc. http://www.wso2.org
-REM
-REM  Licensed under the Apache License, Version 2.0 (the "License");
-REM  you may not use this file except in compliance with the License.
-REM  You may obtain a copy of the License at
-REM
-REM      http://www.apache.org/licenses/LICENSE-2.0
-REM
-REM  Unless required by applicable law or agreed to in writing, software
-REM  distributed under the License is distributed on an "AS IS" BASIS,
-REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM  See the License for the specific language governing permissions and
-REM  limitations under the License.
-
 rem ---------------------------------------------------------------------------
-rem Main Script for WSO2 Carbon
+rem  Licensed to the Apache Software Foundation (ASF) under one
+rem  or more contributor license agreements.  See the NOTICE file
+rem  distributed with this work for additional information
+rem  regarding copyright ownership.  The ASF licenses this file
+rem  to you under the Apache License, Version 2.0 (the
+rem  "License"); you may not use this file except in compliance
+rem  with the License.  You may obtain a copy of the License at
 rem
-rem Environment Variable Prequisites
+rem    http://www.apache.org/licenses/LICENSE-2.0
 rem
-rem   CARBON_HOME   Home of CARBON installation. If not set I will  try
-rem                   to figure it out.
+rem  Unless required by applicable law or agreed to in writing,
+rem  software distributed under the License is distributed on an
+rem  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+rem  KIND, either express or implied.  See the License for the
+rem  specific language governing permissions and limitations
+rem  under the License.
+rem ---------------------------------------------------------------------------
+rem  Main Script for Apache Stratos
+rem
+rem  Environment Variable Prerequisites
 rem
 rem   JAVA_HOME       Must point at your Java Development Kit installation.
 rem
