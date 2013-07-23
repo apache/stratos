65a93d776540bc18cc455a0f2050c6aad8addd85
Imesh Gunaratne <imesh@wso2.com>
2013-07-23 11:54:53 +0530

    Updated license header in xml, dtd, xsd, sh, bat and properties files and product name in install.txt
    Signed-off-by: Lakmal Warusawithana <lakmal@wso2.com>

--- products/cloud_controller/modules/distribution/src/main/resources/wso2server.sh
+++ products/cloud_controller/modules/distribution/src/main/resources/wso2server.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 # ----------------------------------------------------------------------------
-#  Copyright 2005-2009 WSO2, Inc. http://www.wso2.org
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 # ----------------------------------------------------------------------------
-# Main Script for the WSO2 Carbon Server
+# Main Script for the Apache Stratos
 #
-# Environment Variable Prequisites
-#
-#   CARBON_HOME   Home of WSO2 Carbon installation. If not set I will  try
-#                   to figure it out.
+# Environment Variable Prerequisites
 #
 #   JAVA_HOME       Must point at your Java Development Kit installation.
 #
