ab056a412e04cd659c2833b367ed3cd885d6231a
Pradeep Fernando <pradeepfn@gmail.com>
2013-11-18 15:46:13 +0530

    minor edit. My first commit and testing...

--- components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/bean/CartridgeInfoBean.java
+++ components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/bean/CartridgeInfoBean.java
@@ -1,4 +1,4 @@
-package org.apache.stratos.rest.endpoint.bean;/*
+/*
 *  Copyright (c) 2005-2012, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 *  WSO2 Inc. licenses this file to you under the Apache License,
@@ -15,6 +15,7 @@ package org.apache.stratos.rest.endpoint.bean;/*
 * specific language governing permissions and limitations
 * under the License.
 */
+package org.apache.stratos.rest.endpoint.bean;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
