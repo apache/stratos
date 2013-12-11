8ca73a341aa7116390fe04cdac7be96b7f4bf7a8
Isuru <isuruh@wso2.com>
2013-12-11 11:25:25 +0530

    adding support to map marshalling/unmarshalling and annotating messaging component

--- components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/bean/util/type/map/MapType.java
+++ components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/bean/util/type/map/MapType.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -17,14 +17,12 @@
  * under the License.
  */
 
-package org.apache.stratos.messaging.domain.topology;
+package org.apache.stratos.rest.endpoint.bean.util.type.map;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Represents status of a member during its lifecycle.
- */
-@XmlRootElement
-public enum MemberStatus {
-    Created, Starting, Activated, Suspended, Terminated
+public class MapType {
+
+    public List<MapEntryType> entry = new ArrayList<MapEntryType>();
 }
