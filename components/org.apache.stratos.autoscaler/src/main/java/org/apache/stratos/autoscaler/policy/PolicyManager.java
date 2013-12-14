apache/stratos

commit 0091a767af458b095f4974f218952c2da2356d77

Refactored autoscaler service component, partition manager, policy manager and registry manager - revision 1

components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/policy/PolicyManager.java
@@ -19,7 +19,6 @@
 
 package org.apache.stratos.autoscaler.policy;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -29,40 +28,35 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.stratos.autoscaler.deployment.policy.DeploymentPolicy;
-import org.apache.stratos.autoscaler.exception.AutoScalerException;
 import org.apache.stratos.autoscaler.exception.InvalidPartitionException;
 import org.apache.stratos.autoscaler.exception.InvalidPolicyException;
-import org.apache.stratos.autoscaler.exception.PolicyValidationException;
 import org.apache.stratos.autoscaler.partition.PartitionManager;
 import org.apache.stratos.autoscaler.policy.model.AutoscalePolicy;
 import org.apache.stratos.autoscaler.registry.RegistryManager;
 import org.apache.stratos.autoscaler.util.AutoScalerConstants;
 import org.apache.stratos.cloud.controller.deployment.partition.Partition;
-import org.wso2.carbon.registry.core.exceptions.RegistryException;
 
 /**
- * 
- *  Manager class for the purpose of managing Autoscale/Deployment policy definitions.
+ * Manager class for the purpose of managing Autoscale/Deployment policy definitions.
  */
 public class PolicyManager {
-	
-	private static final Log log = LogFactory.getLog(PolicyManager.class);
 
-	private static final String asResourcePath = AutoScalerConstants.AUTOSCALER_RESOURCE+ AutoScalerConstants.AS_POLICY_RESOURCE + "/";
-	private static final String deploymentPolicyResourcePath = AutoScalerConstants.AUTOSCALER_RESOURCE+ AutoScalerConstants.DEPLOYMENT_POLICY_RESOURCE + "/";
-	
-	private static Map<String,AutoscalePolicy> autoscalePolicyListMap = new HashMap<String, AutoscalePolicy>();
-	
-	private static Map<String,DeploymentPolicy> deploymentPolicyListMap = new HashMap<String, DeploymentPolicy>();
-	
-	private static PolicyManager instance = null;
-	 
+    private static final Log log = LogFactory.getLog(PolicyManager.class);
+
+    private static final String asResourcePath = AutoScalerConstants.AUTOSCALER_RESOURCE + AutoScalerConstants.AS_POLICY_RESOURCE + "/";
+
+    private static Map<String, AutoscalePolicy> autoscalePolicyListMap = new HashMap<String, AutoscalePolicy>();
+
+    private static Map<String, DeploymentPolicy> deploymentPolicyListMap = new HashMap<String, DeploymentPolicy>();
+
+    private static PolicyManager instance = null;
+
     private PolicyManager() {
     }
 
     public static PolicyManager getInstance() {
         if (instance == null) {
-            synchronized (PolicyManager.class){
+            synchronized (PolicyManager.class) {
                 if (instance == null) {
                     instance = new PolicyManager();
                 }
@@ -70,167 +64,158 @@ public class PolicyManager {
         }
         return instance;
     }
-    
+
     // Add the policy to information model and persist.
-	public boolean deployAutoscalePolicy(AutoscalePolicy policy) throws InvalidPolicyException {	
-		this.addASPolicyToInformationModel(policy);
-		this.persitASPolicy(asResourcePath+ policy.getId(), policy);	
-		
-		log.info("AutoScaling policy  :" + policy.getId() + " is deployed successfully.");
-		return true;
-	}
-	
-	// Add the deployment policy to information model and persist.
-	public boolean deployDeploymentPolicy(DeploymentPolicy policy) throws InvalidPolicyException {	
-	    try {
-	        Partition[] allPartitions = policy.getAllPartitions();
-            validateExistenceOfPartions(allPartitions);
-            
-        } catch (InvalidPartitionException e) {
-            String msg = "Deployment Policy is invalid. Policy name: " + policy.getId();
-            log.error(msg, e);
-            throw new InvalidPolicyException(msg, e);
+    public boolean deployAutoscalePolicy(AutoscalePolicy policy) throws InvalidPolicyException {
+        this.addASPolicyToInformationModel(policy);
+        RegistryManager.getInstance().persistAutoscalerPolicy(policy);
+        if (log.isInfoEnabled()) {
+            log.info(String.format("AutoScaling policy is deployed successfully: [id] %s", policy.getId()));
         }
-	    
-		this.addDeploymentPolicyToInformationModel(policy);
-		this.persitDeploymentPolicy(deploymentPolicyResourcePath+ policy.getId(), policy);
-		
-		log.info("Deployment policy  :" + policy.getId() + " is deployed successfully.");
-		return true;
-	}
-	
-	private static void validateExistenceOfPartions(Partition[] partitions) throws InvalidPartitionException {
+        return true;
+    }
+
+    // Add the deployment policy to information model and persist.
+    public boolean deployDeploymentPolicy(DeploymentPolicy policy) throws InvalidPolicyException {
+        try {
+            if(log.isInfoEnabled()) {
+                log.info(String.format("Deploying deployment policy: [id] %s", policy.getId()));
+            }
+            fillPartitions(policy);
+        } catch (InvalidPartitionException e) {
+            throw new InvalidPolicyException(String.format("Deployment policy is invalid: [id] %s", policy.getId()), e);
+        }
+
+        this.addDeploymentPolicyToInformationModel(policy);
+        RegistryManager.getInstance().persistDeploymentPolicy(policy);
+
+        if (log.isInfoEnabled()) {
+            log.info(String.format("Deployment policy is deployed successfully: [id] %s", policy.getId()));
+        }
+        return true;
+    }
+
+    private void fillPartitions(DeploymentPolicy deploymentPolicy) throws InvalidPartitionException {
         PartitionManager partitionMgr = PartitionManager.getInstance();
-        for (Partition partition : partitions) {
+        for (Partition partition : deploymentPolicy.getAllPartitions()) {
             String partitionId = partition.getId();
-            if (partitionId == null || !partitionMgr.partitionExist(partitionId)) {
-                String msg =
-                             "Non existing Partition defined. Partition id: " + partitionId + ". " +
-                                     "Please define the partition in the partition definition file.";
-                log.error(msg);
+            if ((partitionId == null) || (!partitionMgr.partitionExist(partitionId))) {
+                String msg = "Could not find partition: [id] " + partitionId + ". " +
+                        "Please deploy the partitions before deploying the deployment policies.";
                 throw new InvalidPartitionException(msg);
             }
-            fillPartition(partition, partitionMgr.getPartitionById(partitionId));
+            fillPartition(partition, PartitionManager.getInstance().getPartitionById(deploymentPolicy.getId()));
         }
     }
 
     private static void fillPartition(Partition destPartition, Partition srcPartition) {
-    	if(log.isDebugEnabled())
-    		log.debug("Settting provider for Partition " + destPartition.getId() + " provider "+ srcPartition.getProvider());
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("Setting provider for partition: [id] %s [provider] %s", destPartition.getId(), srcPartition.getProvider()));
+        }
         destPartition.setProvider(srcPartition.getProvider());
-        
-        if(log.isDebugEnabled())
-        	log.info("Settting properties for Partition " + destPartition.getId() + " provider "+ srcPartition.getProperties());
+
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("Setting properties for partition: [id] %s [properties] %s", destPartition.getId(), srcPartition.getProperties()));
+        }
         destPartition.setProperties(srcPartition.getProperties());
-
     }
-		
-	public void addASPolicyToInformationModel(AutoscalePolicy asPolicy) throws InvalidPolicyException{
-		if (!autoscalePolicyListMap.containsKey(asPolicy.getId())) {
-			if(log.isDebugEnabled()){
-				log.debug("Adding policy :" + asPolicy.getId());
-			}			
-			autoscalePolicyListMap.put(asPolicy.getId(), asPolicy);
-		} else {
-			throw new InvalidPolicyException("Specified policy [" + asPolicy.getId()
-					+ "] already exists");
-		}
-	}
-	
-	private void persitASPolicy(String asResourcePath, AutoscalePolicy policy){		
-		try {
-			RegistryManager.getInstance().persist(policy, asResourcePath);
-		} catch (RegistryException e) {
-			throw new AutoScalerException(e);
-		}
-	}
-	
-	private void persitDeploymentPolicy(String depResourcePath, DeploymentPolicy policy){		
-		try {
-			RegistryManager.getInstance().persist(policy, depResourcePath);
-		} catch (RegistryException e) {
-			throw new AutoScalerException(e);
-		}
-	}
-	
-	/**
-	 * Removes the specified policy
-	 * @param policy
-	 * @throws InvalidPolicyException
-	 */
-	public void removeAutoscalePolicy(String policy) throws InvalidPolicyException {
-		if (autoscalePolicyListMap.containsKey(policy)) {
-			if(log.isDebugEnabled()){
-				log.debug("Removing policy :" + policy);
-			}
-			autoscalePolicyListMap.remove(policy);
-		} else {
-			throw new InvalidPolicyException("No such policy [" + policy + "] exists");
-		}
-	}
-	
-	/**
-	 * Returns a List of the Autoscale policies contained in this manager.
-	 * @return
-	 */
-	public List<AutoscalePolicy> getAutoscalePolicyList() {
-		return Collections.unmodifiableList(new ArrayList<AutoscalePolicy>(autoscalePolicyListMap.values()));
-	}
-	
-	/**
-	 * Returns the autoscale policy to which the specified id is mapped or null
-	 * @param id
-	 * @return
-	 */
-	public AutoscalePolicy getAutoscalePolicy(String id) {
-		return autoscalePolicyListMap.get(id);
-	}
 
-	// Add the deployment policy to As in memmory information model. Does not persist.
-	public void addDeploymentPolicyToInformationModel(DeploymentPolicy policy) throws InvalidPolicyException {
-		if (!deploymentPolicyListMap.containsKey(policy.getId())) {
-			if(log.isDebugEnabled()){
-				log.debug("Adding policy :" + policy.getId());
-			}
-			PartitionManager.getInstance().deployNewNetworkPartitions(policy);
-			deploymentPolicyListMap.put(policy.getId(), policy);
-		} else {
-			throw new InvalidPolicyException("Specified policy [" + policy.getId()
-					+ "] already exists");
-		}
-	}
-		
-	/**
-	 * Removes the specified policy
-	 * @param policy
-	 * @throws InvalidPolicyException
-	 */
-	public void removeDeploymentPolicy(String policy) throws InvalidPolicyException {
-		if (deploymentPolicyListMap.containsKey(policy)) {
-			if(log.isDebugEnabled()){
-				log.debug("Removing policy :" + policy);
-			}
-			deploymentPolicyListMap.remove(policy);
-		} else {
-			throw new InvalidPolicyException("No such policy [" + policy + "] exists");
-		}
-	}
-	
-	/**
-	 * Returns a List of the Deployment policies contained in this manager.
-	 * @return
-	 */
-	public List<DeploymentPolicy> getDeploymentPolicyList() {
-		return Collections.unmodifiableList(new ArrayList<DeploymentPolicy>(deploymentPolicyListMap.values()));
-	}
-	
-	/**
-	 * Returns the deployment policy to which the specified id is mapped or null
-	 * @param id
-	 * @return
-	 */
-	public DeploymentPolicy getDeploymentPolicy(String id) {
-		return deploymentPolicyListMap.get(id);
-	}
+    public void addASPolicyToInformationModel(AutoscalePolicy asPolicy) throws InvalidPolicyException {
+        if (!autoscalePolicyListMap.containsKey(asPolicy.getId())) {
+            if (log.isDebugEnabled()) {
+                log.debug("Adding policy :" + asPolicy.getId());
+            }
+            autoscalePolicyListMap.put(asPolicy.getId(), asPolicy);
+        } else {
+            throw new InvalidPolicyException("Specified policy [" + asPolicy.getId()
+                    + "] already exists");
+        }
+    }
+
+    /**
+     * Removes the specified policy
+     *
+     * @param policy
+     * @throws InvalidPolicyException
+     */
+    public void removeAutoscalePolicy(String policy) throws InvalidPolicyException {
+        if (autoscalePolicyListMap.containsKey(policy)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Removing policy :" + policy);
+            }
+            autoscalePolicyListMap.remove(policy);
+        } else {
+            throw new InvalidPolicyException("No such policy [" + policy + "] exists");
+        }
+    }
+
+    /**
+     * Returns a List of the Autoscale policies contained in this manager.
+     *
+     * @return
+     */
+    public List<AutoscalePolicy> getAutoscalePolicyList() {
+        return Collections.unmodifiableList(new ArrayList<AutoscalePolicy>(autoscalePolicyListMap.values()));
+    }
+
+    /**
+     * Returns the autoscale policy to which the specified id is mapped or null
+     *
+     * @param id
+     * @return
+     */
+    public AutoscalePolicy getAutoscalePolicy(String id) {
+        return autoscalePolicyListMap.get(id);
+    }
+
+    // Add the deployment policy to As in memmory information model. Does not persist.
+    public void addDeploymentPolicyToInformationModel(DeploymentPolicy policy) throws InvalidPolicyException {
+        if (!deploymentPolicyListMap.containsKey(policy.getId())) {
+            if (log.isDebugEnabled()) {
+                log.debug("Adding policy :" + policy.getId());
+            }
+            PartitionManager.getInstance().deployNewNetworkPartitions(policy);
+            deploymentPolicyListMap.put(policy.getId(), policy);
+        } else {
+            throw new InvalidPolicyException("Specified policy [" + policy.getId()
+                    + "] already exists");
+        }
+    }
+
+    /**
+     * Removes the specified policy
+     *
+     * @param policy
+     * @throws InvalidPolicyException
+     */
+    public void removeDeploymentPolicy(String policy) throws InvalidPolicyException {
+        if (deploymentPolicyListMap.containsKey(policy)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Removing policy :" + policy);
+            }
+            deploymentPolicyListMap.remove(policy);
+        } else {
+            throw new InvalidPolicyException("No such policy [" + policy + "] exists");
+        }
+    }
+
+    /**
+     * Returns a List of the Deployment policies contained in this manager.
+     *
+     * @return
+     */
+    public List<DeploymentPolicy> getDeploymentPolicyList() {
+        return Collections.unmodifiableList(new ArrayList<DeploymentPolicy>(deploymentPolicyListMap.values()));
+    }
+
+    /**
+     * Returns the deployment policy to which the specified id is mapped or null
+     *
+     * @param id
+     * @return
+     */
+    public DeploymentPolicy getDeploymentPolicy(String id) {
+        return deploymentPolicyListMap.get(id);
+    }
 
 }

components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/internal/AutoscalerServerComponent.java
@@ -41,208 +41,93 @@ import org.wso2.carbon.registry.core.service.RegistryService;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
-* @scr.component name=org.apache.stratos.autoscaler.internal.AutoscalerServerComponent"
-* immediate="true"
-*
-* @scr.reference name="registry.service"
-*                interface=
-*                "org.wso2.carbon.registry.core.service.RegistryService"
-*                cardinality="1..1" policy="dynamic" bind="setRegistryService"
-*                unbind="unsetRegistryService"
-*/
+ * @scr.component name=org.apache.stratos.autoscaler.internal.AutoscalerServerComponent"
+ * immediate="true"
+ * @scr.reference name="registry.service"
+ * interface=
+ * "org.wso2.carbon.registry.core.service.RegistryService"
+ * cardinality="1..1" policy="dynamic" bind="setRegistryService"
+ * unbind="unsetRegistryService"
+ */
 
 public class AutoscalerServerComponent {
 
     private static final Log log = LogFactory.getLog(AutoscalerServerComponent.class);
-    private RegistryManager registryManager;
 
     protected void activate(ComponentContext componentContext) throws Exception {
-
-        // Subscribe to all topics
-//        TopicSubscriber topologyTopicSubscriber = new TopicSubscriber(Constants.TOPOLOGY_TOPIC);
-//        topologyTopicSubscriber.setMessageListener(new TopologyEventMessageReceiver());
-//        Thread topologyTopicSubscriberThread = new Thread(topologyTopicSubscriber);
-//        topologyTopicSubscriberThread.start();
-//        if (log.isDebugEnabled()) {
-//            log.debug("Topology event message receiver thread started");
-//        }
-//
-//        TopologyEventMessageDelegator tropologyEventMessageDelegator = new TopologyEventMessageDelegator();
-//        Thread tropologyDelegatorThread = new Thread(tropologyEventMessageDelegator);
-//        tropologyDelegatorThread.start();
-
-        
-        
-        // Adding the registry stored AS policies to the information model.
-        
         try {
-			Thread topologyTopicSubscriberThread = new Thread(new AutoscalerTopologyReceiver());
-			topologyTopicSubscriberThread.start();
-			if (log.isDebugEnabled()) {
-				log.debug("Topology message processor thread started");
-			}
+            // Start topology receiver
+            Thread topologyTopicSubscriberThread = new Thread(new AutoscalerTopologyReceiver());
+            topologyTopicSubscriberThread.start();
+            if (log.isDebugEnabled()) {
+                log.debug("Topology receiver thread started");
+            }
 
-			TopicSubscriber healthStatTopicSubscriber = new TopicSubscriber(
-					Constants.HEALTH_STAT_TOPIC);
-			healthStatTopicSubscriber
-					.setMessageListener(new HealthEventMessageReceiver());
-			Thread healthStatTopicSubscriberThread = new Thread(
-					healthStatTopicSubscriber);
-			healthStatTopicSubscriberThread.start();
-			if (log.isDebugEnabled()) {
-				log.debug("Health Stat event message receiver thread started");
-			}
-			HealthEventMessageDelegator healthEventMessageDelegator = new HealthEventMessageDelegator();
-			Thread healthDelegatorThread = new Thread(
-					healthEventMessageDelegator);
-			healthDelegatorThread.start();
-			if (log.isDebugEnabled()) {
-				log.debug("Health message processor thread started");
-			}
-			// Start scheduler for running rules
-//			ExecutorTaskScheduler executor = new ExecutorTaskScheduler();
-//			Thread executorThread = new Thread(executor);
-//			executorThread.start();
-//			if (log.isDebugEnabled()) {
-//				log.debug("Rules executor thread started");
-//			}
-			this.registryManager = RegistryManager.getInstance();
-			// Adding the registry stored partitions to the information model.
-			ArrayList<Partition> partitions = this.retreivePartitions();
-			Iterator<Partition> it = partitions.iterator();
-			while (it.hasNext()) {
-				Partition par = it.next();
-				PartitionManager.getInstance().addPartitionToInformationModel(
-						par);
-			}
-			ArrayList<AutoscalePolicy> asPolicies = this.retreiveASPolicies();
-			Iterator<AutoscalePolicy> asItr = asPolicies.iterator();
-			while (asItr.hasNext()) {
-				AutoscalePolicy asPolicy = asItr.next();
-				PolicyManager.getInstance().addASPolicyToInformationModel(asPolicy);
-			}
-			
-			ArrayList<DeploymentPolicy> depPolicies = this.retreiveDeploymentPolicies();
-			Iterator<DeploymentPolicy> depItr = depPolicies.iterator();
-			while (depItr.hasNext()) {
-				DeploymentPolicy depPolicy = depItr.next();
-				PolicyManager.getInstance().addDeploymentPolicyToInformationModel(depPolicy);
-			}
-			
-			if (log.isInfoEnabled()) {
-				log.info("Autoscaler Server Component activated");
-			}
-		} catch (Throwable e) {
-			log.error("Error in activating the autoscaler component ", e);
-		}
-    }
-    
-    protected void setRegistryService(RegistryService registryService) {
-		if (log.isDebugEnabled()) {
-			log.debug("Setting the Registry Service");
-		}
-		try {
-	        ServiceReferenceHolder.getInstance().setRegistry(registryService.getGovernanceSystemRegistry());
-        } catch (RegistryException e) {
-        	String msg = "Failed when retrieving Governance System Registry.";
-        	log.error(msg, e);
-        	throw new AutoScalerException(msg, e);
+            // Start health stat receiver
+            TopicSubscriber healthStatTopicSubscriber = new TopicSubscriber(Constants.HEALTH_STAT_TOPIC);
+            healthStatTopicSubscriber.setMessageListener(new HealthEventMessageReceiver());
+            Thread healthStatTopicSubscriberThread = new Thread(healthStatTopicSubscriber);
+            healthStatTopicSubscriberThread.start();
+            if (log.isDebugEnabled()) {
+                log.debug("Health event message receiver thread started");
+            }
+
+            HealthEventMessageDelegator healthEventMessageDelegator = new HealthEventMessageDelegator();
+            Thread healthDelegatorThread = new Thread(healthEventMessageDelegator);
+            healthDelegatorThread.start();
+            if (log.isDebugEnabled()) {
+                log.debug("Health message processor thread started");
+            }
+
+            // Adding the registry stored partitions to the information model
+            List<Partition> partitions = RegistryManager.getInstance().retrievePartitions();
+            Iterator<Partition> partitionIterator = partitions.iterator();
+            while (partitionIterator.hasNext()) {
+                Partition partition = partitionIterator.next();
+                PartitionManager.getInstance().addPartitionToInformationModel(partition);
+            }
+            List<AutoscalePolicy> asPolicies = RegistryManager.getInstance().retrieveASPolicies();
+            Iterator<AutoscalePolicy> asPolicyIterator = asPolicies.iterator();
+            while (asPolicyIterator.hasNext()) {
+                AutoscalePolicy asPolicy = asPolicyIterator.next();
+                PolicyManager.getInstance().addASPolicyToInformationModel(asPolicy);
+            }
+
+            List<DeploymentPolicy> depPolicies = RegistryManager.getInstance().retrieveDeploymentPolicies();
+            Iterator<DeploymentPolicy> depPolicyIterator = depPolicies.iterator();
+            while (depPolicyIterator.hasNext()) {
+                DeploymentPolicy depPolicy = depPolicyIterator.next();
+                PolicyManager.getInstance().addDeploymentPolicyToInformationModel(depPolicy);
+            }
+
+            if (log.isInfoEnabled()) {
+                log.info("Autoscaler Server Component activated");
+            }
+        } catch (Throwable e) {
+            log.error("Error in activating the autoscaler component ", e);
         }
-	 }
+    }
 
-	protected void unsetRegistryService(RegistryService registryService) {
-		if (log.isDebugEnabled()) {
+    protected void setRegistryService(RegistryService registryService) {
+        if (log.isDebugEnabled()) {
+            log.debug("Setting the Registry Service");
+        }
+        try {
+            ServiceReferenceHolder.getInstance().setRegistry(registryService.getGovernanceSystemRegistry());
+        } catch (RegistryException e) {
+            String msg = "Failed when retrieving Governance System Registry.";
+            log.error(msg, e);
+            throw new AutoScalerException(msg, e);
+        }
+    }
+
+    protected void unsetRegistryService(RegistryService registryService) {
+        if (log.isDebugEnabled()) {
             log.debug("Unsetting the Registry Service");
         }
         ServiceReferenceHolder.getInstance().setRegistry(null);
-	}
-	
-	private ArrayList<Partition> retreivePartitions(){
-		ArrayList<Partition> partitionList = new ArrayList<Partition>();
-		 String [] partitionsResourceList = (String [])registryManager.retrieve(AutoScalerConstants.AUTOSCALER_RESOURCE + AutoScalerConstants.PARTITION_RESOURCE);
-		 
-	        if (partitionsResourceList != null) {
-				for (String resourcePath : partitionsResourceList) {
-					Object partition = registryManager.retrieve(resourcePath);
-
-					if (partition != null) {
-						try {
-
-							Object dataObj = Deserializer
-									.deserializeFromByteArray((byte[]) partition);
-							if (dataObj instanceof Partition) {
-								partitionList.add((Partition) dataObj);
-							} else {
-								return null;
-							}
-						} catch (Exception e) {
-							String msg = "Unable to retrieve data from Registry. Hence, any historical partitions will not get reflected.";
-							log.warn(msg, e);
-						}
-					}
-				}
-			}
-			return partitionList;	        
-	}
-	
-	private ArrayList<AutoscalePolicy> retreiveASPolicies(){
-		ArrayList<AutoscalePolicy> asPolicyList = new ArrayList<AutoscalePolicy>();
-		 String [] partitionsResourceList = (String [])registryManager.retrieve(AutoScalerConstants.AUTOSCALER_RESOURCE + AutoScalerConstants.AS_POLICY_RESOURCE);
-		 
-	        if (partitionsResourceList != null) {
-				for (String resourcePath : partitionsResourceList) {
-					Object asPolicy = registryManager.retrieve(resourcePath);
-
-					if (asPolicy != null) {
-						try {
-
-							Object dataObj = Deserializer
-									.deserializeFromByteArray((byte[]) asPolicy);
-							if (dataObj instanceof AutoscalePolicy) {
-								asPolicyList.add((AutoscalePolicy) dataObj);
-							} else {
-								return null;
-							}
-						} catch (Exception e) {
-							String msg = "Unable to retrieve data from Registry. Hence, any historical autoscaler policies will not get reflected.";
-							log.warn(msg, e);
-						}
-					}
-				}
-			}
-			return asPolicyList;	        
-	}
-	
-	private ArrayList<DeploymentPolicy> retreiveDeploymentPolicies(){
-		ArrayList<DeploymentPolicy> depPolicyList = new ArrayList<DeploymentPolicy>();
-		 String [] depPolicyResourceList = (String [])registryManager.retrieve(AutoScalerConstants.AUTOSCALER_RESOURCE + AutoScalerConstants.DEPLOYMENT_POLICY_RESOURCE);
-		 
-	        if (depPolicyResourceList != null) {
-				for (String resourcePath : depPolicyResourceList) {
-					Object asPolicy = registryManager.retrieve(resourcePath);
-
-					if (asPolicy != null) {
-						try {
-
-							Object dataObj = Deserializer
-									.deserializeFromByteArray((byte[]) asPolicy);
-							if (dataObj instanceof DeploymentPolicy) {
-								depPolicyList.add((DeploymentPolicy) dataObj);
-							} else {
-								return null;
-							}
-						} catch (Exception e) {
-							String msg = "Unable to retrieve data from Registry. Hence, any historical deployment policies will not get reflected.";
-							log.warn(msg, e);
-						}
-					}
-				}
-			}
-			return depPolicyList;	        
-	}
-	
-	
-	
+    }
 }

commit a1b72d411f4cf09f3972c892f02bb9326e5b02a2

fixing a broken logic by commit  0091a767af458b095f4974f218952c2da2356d77

components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/policy/PolicyManager.java
@@ -104,7 +104,7 @@ public class PolicyManager {
                         "Please deploy the partitions before deploying the deployment policies.";
                 throw new InvalidPartitionException(msg);
             }
-            fillPartition(partition, PartitionManager.getInstance().getPartitionById(deploymentPolicy.getId()));
+            fillPartition(partition, PartitionManager.getInstance().getPartitionById(partitionId));
         }
     }
 