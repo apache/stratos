43debf400479bfc144a49cc7eb1d6ea9d3ebabbc
rekathiru <rthirunavukkarasu23@gmail.com>
2014-01-29 19:46:28 +0530

    adding support for listing the memebers

--- components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/beans/topology/Cluster.java
+++ components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/beans/topology/Cluster.java
@@ -1,49 +1,50 @@
 package org.apache.stratos.cli.beans.topology;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.*;
+import java.util.List;
 
 public class Cluster {
-    private static final long serialVersionUID = -361960242360176077L;
+    private String serviceName;
 
-	private final String serviceName;
-    private final String clusterId;
-    private final String autoscalePolicyName;
-    private final String deploymentPolicyName;
+    private String clusterId;
+
+    private List<Member> member;
+
+    private String tenantRange;
 
     private List<String> hostNames;
-    private String tenantRange;
+
     private boolean isLbCluster;
-    // Key: Member.memberId
-    private Map<String, Member> memberMap;
-
-    private String loadBalanceAlgorithmName;
-    private Properties properties;
-
-    public Cluster(String serviceName, String clusterId, String deploymentPolicyName, String autoscalePolicyName) {
-        this.serviceName = serviceName;
-        this.clusterId = clusterId;
-        this.deploymentPolicyName = deploymentPolicyName;
-        this.autoscalePolicyName = autoscalePolicyName;
-        this.hostNames = new ArrayList<String>();
-        this.memberMap = new HashMap<String, Member>();
-    }
 
     public String getServiceName() {
         return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
     }
 
     public String getClusterId() {
         return clusterId;
     }
 
-    public List<String> getHostNames() {
-        return hostNames;
+    public void setClusterId(String clusterId) {
+        this.clusterId = clusterId;
     }
 
-    public void addHostName(String hostName) {
-        this.hostNames.add(hostName);
+    public List<Member> getMember() {
+        return member;
+    }
+
+    public void addMember(Member member) {
+       this.member.add(member);
+    }
+
+    public void removeMember(Member member) {
+       this.member.remove(member);
+    }
+
+    public void setMember(List<Member> member) {
+        this.member = member;
     }
 
     public String getTenantRange() {
@@ -54,117 +55,27 @@ public class Cluster {
         this.tenantRange = tenantRange;
     }
 
-    public Collection<Member> getMembers() {
-        return memberMap.values();
+    public List<String> getHostNames() {
+        return hostNames;
     }
 
-    public boolean hasMembers() {
-        return  memberMap.isEmpty();
+    public void setHostNames(List<String> hostNames) {
+        this.hostNames = hostNames;
     }
 
-
-
-    public void addMember(Member member) {
-        memberMap.put(member.getMemberId(), member);
+    public void addHostNames(String hostName) {
+        this.hostNames.add(hostName);
     }
 
-    public void removeMember(Member member) {
-        memberMap.remove(member.getMemberId());
-    }
-
-    public Member getMember(String memberId) {
-        return memberMap.get(memberId);
-    }
-
-    public boolean memberExists(String memberId) {
-        return this.memberMap.containsKey(memberId);
-    }
-
-    public Properties getProperties() {
-        return properties;
-    }
-
-    public void setProperties(Properties properties) {
-        this.properties = properties;
-    }
-
-    public String getAutoscalePolicyName() {
-        return autoscalePolicyName;
-    }
-
-	public String getDeploymentPolicyName() {
-		return deploymentPolicyName;
-	}
-
-    public String getLoadBalanceAlgorithmName() {
-        return loadBalanceAlgorithmName;
-    }
-
-    public void setLoadBalanceAlgorithmName(String loadBalanceAlgorithmName) {
-        this.loadBalanceAlgorithmName = loadBalanceAlgorithmName;
+    public void removeHostNames(String hostName) {
+        this.hostNames.remove(hostName);
     }
 
     public boolean isLbCluster() {
         return isLbCluster;
     }
 
-    public void setLbCluster(boolean isLbCluster) {
-        this.isLbCluster = isLbCluster;
-    }
-
-    @Override
-    public String toString() {
-        return "Cluster [serviceName=" + serviceName + ", clusterId=" + clusterId +
-                ", autoscalePolicyName=" + autoscalePolicyName + ", deploymentPolicyName=" +
-                deploymentPolicyName + ", hostNames=" + hostNames + ", tenantRange=" + tenantRange +
-                ", isLbCluster=" + isLbCluster + ", properties=" + properties + "]";
-    }
-
-    /**
-     * Check whether a given tenant id is in tenant range of the cluster.
-     *
-     * @param tenantId
-     * @return
-     */
-    public boolean tenantIdInRange(int tenantId) {
-        if(StringUtils.isBlank(getTenantRange())) {
-            return false;
-        }
-
-        if("*".equals(getTenantRange())) {
-            return true;
-        }
-        else {
-            String[] array = getTenantRange().split("-");
-            int tenantStart = Integer.parseInt(array[0]);
-            if(tenantStart <= tenantId) {
-                String tenantEndStr = array[1];
-                if("*".equals(tenantEndStr)) {
-                    return true;
-                }
-                else {
-                    int tenantEnd = Integer.parseInt(tenantEndStr);
-                    if(tenantId <= tenantEnd) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Find partitions used by the cluster and return their ids as a collection.
-     *
-     * @return
-     */
-    public Collection<String> findPartitionIds() {
-        Map<String, Boolean> partitionIds = new HashMap<String, Boolean>();
-        for(Member member : getMembers()) {
-            if((StringUtils.isNotBlank(member.getPartitionId())) && (!partitionIds.containsKey(member.getPartitionId()))) {
-                partitionIds.put(member.getPartitionId(), true);
-            }
-        }
-        return partitionIds.keySet();
+    public void setLbCluster(boolean lbCluster) {
+        isLbCluster = lbCluster;
     }
 }
