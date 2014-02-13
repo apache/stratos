7235ffd0ad77fbff3a0c6166f8e3b5583e4df2fb
Manula Thantriwatte <manula@apache.org>
2014-02-13 16:10:07 +0530

    Comment deploy service, undeploy service, list deploy service commands in CLI

--- components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/StratosApplication.java
+++ components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/StratosApplication.java
@@ -120,14 +120,14 @@ public class StratosApplication extends CommandLineApplication<StratosCommandCon
         command = new AutoscalingPolicyDeploymentCommand();
         commands.put(command.getName(), command);
 
-        command = new DeployServiceDeploymentCommand();
-        commands.put(command.getName(), command);
+        //command = new DeployServiceDeploymentCommand();
+        //commands.put(command.getName(), command);
 
-        command = new UndeployServiceDefinitionCommand();
-        commands.put(command.getName(), command);
+        //command = new UndeployServiceDefinitionCommand();
+        //commands.put(command.getName(), command);
 
-        command = new ListDeployServiceCommand();
-        commands.put(command.getName(), command);
+        //command = new ListDeployServiceCommand();
+        //commands.put(command.getName(), command);
 
         command = new UndeployCartridgeDefinitionCommand();
         commands.put(command.getName(), command);
