a037318a6e4821296396a053cbf127ec68c65714
rekathiru <rthirunavukkarasu23@gmail.com>
2014-02-14 12:55:01 +0530

    fixing error handling, cartridge category issues and adding missed new files

---
+++ components/org.apache.stratos.manager.console/console/themes/theme1/ui/js/mycartridges.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('#unsubscribeBtn').click(function () {
+        $('#alias').val($(this).attr('data-alias'));
+        popbox.message({content: '<div>Un-subscribe will delete all your instances.</div><div>Are you sure you want to un-subscribe?</div>', type: 'confirm',
+            okCallback: function () {
+                $('#cForm').submit();
+            },
+            cancelCallback: function () {
+            }
+        });
+    });
+});
