e2924233deee2fc0bc866514a44a4b891f8ad905
Pradeep Fernando <pradeepfn@gmail.com>
2014-04-22 12:49:53 +0530

    adding throwable exception mapper. Now we return json error objects in all cases.

--- components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/handlers/CustomThrowableExceptionMapper.java
+++ components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/handlers/CustomThrowableExceptionMapper.java
@@ -1,4 +1,4 @@
-/*
+package org.apache.stratos.rest.endpoint.handlers;/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,31 +16,24 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.stratos.rest.endpoint.handlers;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.stratos.rest.endpoint.Utils;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
-/*This class maps any exception thrown by the server, which is not mapped by a specifi exception mapper
-* in to an appropriate format
-* */
-public class GenericExceptionMapper implements ExceptionMapper<WebApplicationException> {
-    private static Log log = LogFactory.getLog(GenericExceptionMapper.class);
+public class CustomThrowableExceptionMapper implements ExceptionMapper<Throwable> {
+    private static Log log = LogFactory.getLog(CustomThrowableExceptionMapper.class);
 
-    public Response toResponse(WebApplicationException webApplicationException) {
+    public Response toResponse(Throwable throwable) {
         if(log.isDebugEnabled()){
-            log.debug("Internal erver error", webApplicationException);
+            log.debug("Internal server error", throwable);
         }
-        // if no specific error message specified, spitting out a generaic error message
-        String errorMessage = (webApplicationException.getMessage() != null)?
-                webApplicationException.getMessage():"Internal server error";
+
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON).
-                entity(Utils.buildMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), errorMessage)).build();
+                entity(Utils.buildMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Internal server error")).build();
     }
 }
