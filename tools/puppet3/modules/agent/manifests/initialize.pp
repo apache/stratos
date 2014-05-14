a56ab6f6993407ab0287e4d073e1f09e0fed03e0
Imesh Gunaratne <imesh@apache.org>
2014-05-14 10:38:18 +0800

    Reverted back artifact versions of dependent components and fixed cartridge agent package name in puppet scripts

--- tools/puppet3/modules/agent/manifests/initialize.pp
+++ tools/puppet3/modules/agent/manifests/initialize.pp
@@ -23,29 +23,29 @@ define agent::initialize ($repo, $version, $service, $local_dir, $target, $owner
       ensure => present;
   }
   file {
-    "/${local_dir}/apache-stratos-${service}-${version}-bin.zip":
+    "/${local_dir}/apache-stratos-${service}-${version}.zip":
       ensure => present,
-      source => "puppet:///modules/agent/apache-stratos-${service}-${version}-bin.zip",
+      source => "puppet:///modules/agent/apache-stratos-${service}-${version}.zip",
       require => File["/${local_dir}/"];
   }
   exec {
     "creating_target_for_${name}":
       path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       command => "mkdir -p ${target}",
-      require => File["/${local_dir}/apache-stratos-${service}-${version}-bin.zip"];
+      require => File["/${local_dir}/apache-stratos-${service}-${version}.zip"];
 
     "creating_local_package_repo_for_${name}":
       path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/java/bin/',
       unless  => "test -d ${local_dir}",
       command => "mkdir -p ${local_dir}";
 
-    "downloading_apache-stratos-${service}-${version}-bin.zip":
+    "downloading_apache-stratos-${service}-${version}.zip":
       path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       cwd       => $local_dir,
-      unless    => "test -f ${local_dir}/apache-stratos-${service}-${version}-bin.zip",
-      command   => "puppet:///modules/agent/apache-stratos-${service}-${version}-bin.zip",
+      unless    => "test -f ${local_dir}/apache-stratos-${service}-${version}.zip",
+      command   => "puppet:///modules/agent/apache-stratos-${service}-${version}.zip",
       logoutput => 'on_failure',
-      creates   => "${local_dir}/apache-stratos-${service}-${version}-bin.zip",
+      creates   => "${local_dir}/apache-stratos-${service}-${version}.zip",
       timeout   => 10,
       require   => Exec["creating_local_package_repo_for_${name}", "creating_target_for_${name}"];
 
@@ -53,11 +53,11 @@ define agent::initialize ($repo, $version, $service, $local_dir, $target, $owner
       path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       cwd       => $target,
       unless    => "test -d ${target}/apache-stratos-${service}-${version}/conf",
-      command   => "unzip ${local_dir}/apache-stratos-${service}-${version}-bin.zip",
+      command   => "unzip ${local_dir}/apache-stratos-${service}-${version}.zip",
       logoutput => 'on_failure',
       creates   => "${target}/apache-stratos-${service}-${version}/repository",
       timeout   => 0,
-      require   => Exec["downloading_apache-stratos-${service}-${version}-bin.zip"];
+      require   => Exec["downloading_apache-stratos-${service}-${version}.zip"];
 
     "setting_permission_for_${name}":
       path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
