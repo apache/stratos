352026bb51ce18cb4123899f90afeb2446b66b58
Imesh Gunaratne <imesh@apache.org>
2014-05-23 01:02:09 +0530

    Revert "Fixing https://issues.apache.org/jira/browse/STRATOS-655"
    This reverts commit 631b0b7b30c6b6df8a156258cb973ce34908231f.

--- products/stratos/modules/distribution/src/bin/stratos.sh
+++ products/stratos/modules/distribution/src/bin/stratos.sh
@@ -183,7 +183,7 @@ elif [ "$CMD" = "start" ]; then
   fi
   export CARBON_HOME=$CARBON_HOME
 # using nohup bash to avoid erros in solaris OS.TODO
-  nohup bash $CARBON_HOME/bin/stratos.sh $args < /dev/null > /dev/null 2>&1 &
+  nohup bash $CARBON_HOME/bin/stratos.sh $args > /dev/null 2>&1 &
   exit 0
 elif [ "$CMD" = "stop" ]; then
   export CARBON_HOME=$CARBON_HOME
@@ -202,7 +202,7 @@ elif [ "$CMD" = "restart" ]; then
   done
 
 # using nohup bash to avoid erros in solaris OS.TODO
-  nohup bash $CARBON_HOME/bin/stratos.sh $args < /dev/null > /dev/null 2>&1 &
+  nohup bash $CARBON_HOME/bin/stratos.sh $args > /dev/null 2>&1 &
   exit 0
 elif [ "$CMD" = "test" ]; then
     JAVACMD="exec "$JAVACMD""
