d5245061d66a5ea9ca721e05d9238feb7a797b9e
Chamila de Alwis <chamilad@wso2.com>
2014-10-01 19:51:24 +0530

    Introduced LogFactory to centralize log management

---
+++ tools/python-cartridge-agent/cartridge-agent/modules/util/log.py
@@ -0,0 +1,38 @@
+import logging
+import logging.config
+import os
+
+
+class LogFactory(object):
+    """
+    Singleton implementation for handling logging in CartridgeAgent
+    """
+    class __LogFactory:
+        def __init__(self):
+            self.logs = {}
+            logging_conf = os.path.join(os.path.dirname(__file__), "logging.ini")
+            logging.config.fileConfig(logging_conf)
+
+        def get_log(self, name):
+            if name not in self.logs:
+                self.logs[name] = logging.getLogger(name)
+
+            return self.logs[name]
+
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not LogFactory.instance:
+            LogFactory.instance = LogFactory.__LogFactory()
+
+        return LogFactory.instance
+
+    def get_log(self, name):
+        """
+        Returns a logger class with the specified channel name. Creates a new logger if one doesn't exists for the
+        specified channel
+        :param str name: Channel name
+        :return: The logger class
+        :rtype: RootLogger
+        """
+        self.instance.get_log(name)
