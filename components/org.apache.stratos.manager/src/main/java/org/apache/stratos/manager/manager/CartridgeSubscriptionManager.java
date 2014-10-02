38c05a5afbaeb36716a24f1e907a74a67e79f9ef
Udara Liyanage <udara@wso2.com>
2014-10-02 12:12:21 +0530

    Revert "fix for STRATOS-851"
    This reverts commit 8cbf8a922ed4fcab8b72253097eced07eed65388.

--- components/org.apache.stratos.manager/src/main/java/org/apache/stratos/manager/manager/CartridgeSubscriptionManager.java
+++ components/org.apache.stratos.manager/src/main/java/org/apache/stratos/manager/manager/CartridgeSubscriptionManager.java
@@ -608,9 +608,4 @@ public class CartridgeSubscriptionManager {
     	DataInsertionAndRetrievalManager dataInsertionAndRetrievalManager = new DataInsertionAndRetrievalManager();
         return dataInsertionAndRetrievalManager.getCartridgeSubscriptions(tenantId, cartridgeType);
     }
-
-    public Collection<CartridgeSubscription> getCartridgeSubscriptionsForType (String cartridgeType) {
-
-        return new DataInsertionAndRetrievalManager().getCartridgeSubscriptions(cartridgeType);
-    }
 }
