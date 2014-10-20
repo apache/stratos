b5b6fb3fdebb53f4709ba8468c5e376f2ab989c1
Chamila de Alwis <chamilad@wso2.com>
2014-10-20 14:03:36 +0530

    Added asynctask test

--- tools/python-cartridge-agent/test/test_util.py
+++ tools/python-cartridge-agent/test/test_util.py
@@ -1,4 +1,20 @@
 from cartridgeagent.modules.util.asyncscheduledtask import AsyncScheduledTask
+from threading import Thread
+import time
+
+def thread_worker():
+    f = open("asynctest.txt", "w")
+    f.write("%1.4f" % time.time() * 1000)
+    f.close()
 
 def test_async_task():
-    assert True
+    astask = AsyncScheduledTask(2, Thread(thread_worker()))
+    start_time = time.time() * 1000
+    astask.run()
+    time.sleep(3)
+    astask.terminate()
+    f = open("asynctest.txt")
+    end_time = float(f.read())
+    assert (end_time - start_time) >= 2 * 1000, "Task was executed before specified delay"
+
+
