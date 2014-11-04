31c69b39daacdd196b8d7741f5e1652e164bf96a
Asiri LIyana Arachchi <asiriwork@gmail.com>
2014-11-04 23:35:43 +0530

    clearing typos

---
+++ components/org.apache.stratos.messaging/src/main/java/org/apache/stratos/messaging/event/health/stat/AverageRequestsServingCapabilityEvent.java
@@ -0,0 +1,31 @@
+package org.apache.stratos.messaging.event.health.stat;
+
+/**
+ * Created by asiri on 8/10/14.
+ */
+import org.apache.stratos.messaging.event.Event;
+public class AverageRequestsServingCapabilityEvent extends  Event{
+    private final String networkPartitionId;
+    private final String clusterId;
+    private final float value;
+
+    public AverageRequestsServingCapabilityEvent(String networkPartitionId, String clusterId, float value) {
+        this.networkPartitionId = networkPartitionId;
+        this.clusterId = clusterId;
+        this.value = value;
+
+    }
+    public String getClusterId() {
+        return clusterId;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getNetworkPartitionId() {
+        return networkPartitionId;
+    }
+
+
+}
---
+++ components/org.apache.stratos.messaging/src/main/java/org/apache/stratos/messaging/message/processor/health/stat/AverageRequestsServingCapabilityMessageProcessor.java
@@ -0,0 +1,49 @@
+package org.apache.stratos.messaging.message.processor.health.stat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.stratos.messaging.event.health.stat.AverageRequestsServingCapabilityEvent;
+import org.apache.stratos.messaging.message.processor.MessageProcessor;
+import org.apache.stratos.messaging.util.Util;
+
+/**
+ * Created by asiri on 8/15/14.
+ */
+public class AverageRequestsServingCapabilityMessageProcessor  extends MessageProcessor {
+    private static final Log log = LogFactory.getLog(AverageRequestsServingCapabilityMessageProcessor.class);
+
+    private MessageProcessor nextProcessor;
+
+    @Override
+    public void setNext(MessageProcessor nextProcessor) {
+        this.nextProcessor = nextProcessor;
+    }
+
+    @Override
+    public boolean process(String type, String message, Object object) {
+        if (AverageRequestsServingCapabilityEvent.class.getName().equals(type)) {
+
+            // Parse complete message and build event
+            AverageRequestsServingCapabilityEvent event = (AverageRequestsServingCapabilityEvent) Util.jsonToObject(message, AverageRequestsServingCapabilityEvent.class);
+
+            // Notify event listeners
+            notifyEventListeners(event);
+
+            if(log.isDebugEnabled()){
+                log.debug(String.format("%s event processor notified listeners ... " , type));
+            }
+            return true;
+        }
+        else {
+            if(nextProcessor != null) {
+                return nextProcessor.process(type, message, object);
+            }
+            else {
+                throw new RuntimeException(String.format("Failed to process health stat message using available message processors: [type] %s [body] %s", type, message));
+            }
+        }
+    }
+
+
+
+}
