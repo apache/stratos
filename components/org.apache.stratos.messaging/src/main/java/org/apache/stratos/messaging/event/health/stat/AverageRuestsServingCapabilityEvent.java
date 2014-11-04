e1cf308263217696e2dd02d3a8919feb6db04cb8
Asiri LIyana Arachchi <asiriwork@gmail.com>
2014-11-04 22:56:22 +0530

    diff fixing 1

---
+++ components/org.apache.stratos.messaging/src/main/java/org/apache/stratos/messaging/event/health/stat/AverageRuestsServingCapabilityEvent.java
@@ -0,0 +1,32 @@
+package org.apache.stratos.messaging.event.health.stat;
+
+/**
+ * Created by asiri on 8/10/14.
+ */
+import org.apache.stratos.messaging.event.Event;
+
+public class AverageRuestsServingCapabilityEvent extends Event {
+    private final String networkPartitionId;
+    private final String clusterId;
+    private final float value;
+
+    public AverageRuestsServingCapabilityEvent(String networkPartitionId, String clusterId, float value) {
+        this.networkPartitionId = networkPartitionId;
+        this.clusterId = clusterId;
+        this.value = value;
+
+    }
+    public String getClusterId() {
+        return clusterId;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getNetworkPartitionId() {
+        return networkPartitionId;
+    }
+
+
+}
