8e0425fcf9b314dd5242aa165b39b9cba39d56c1
Lahiru Sandaruwan <lahirus@apache.org>
2014-11-08 20:36:19 +0530

    Removing floor and ceiling since we can handle those in one methode

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/algorithm/InstanceCountRoundingAlgorithm.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/algorithm/InstanceCountRoundingAlgorithm.java
@@ -2,16 +2,6 @@ package org.apache.stratos.autoscaler.algorithm;
 
 public class InstanceCountRoundingAlgorithm {
 
-    public double getFloorCount(float requiredInstances){
-
-        return (int)Math.floor(requiredInstances);
-    }
-
-    public int getCeilCount(float requiredInstances){
-
-        return (int)Math.ceil(requiredInstances);
-    }
-
     public int getFractionBasedCount(float requiredInstances, float fraction){
 
         return (requiredInstances - Math.floor(requiredInstances) > fraction) ? (int)Math.ceil(requiredInstances)
