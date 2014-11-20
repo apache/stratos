8c6cb2b7bb337cf3da39736fd8614e05c4bb9470
Isuru Haththotuwa <isuruh@apache.org>
2014-11-20 14:45:40 +0530

    fixing a unwanted if check

--- components/org.apache.stratos.manager/src/main/java/org/apache/stratos/manager/grouping/deployer/DefaultServiceGroupDeployer.java
+++ components/org.apache.stratos.manager/src/main/java/org/apache/stratos/manager/grouping/deployer/DefaultServiceGroupDeployer.java
@@ -262,7 +262,6 @@ public class DefaultServiceGroupDeployer implements ServiceGroupDeployer {
         ServiceGroup servicegroup = new ServiceGroup();
 
         // implement conversion (mostly List -> Array)
-        if (serviceGroupDefinition.isGroupScalingEnabled())
         servicegroup.setGroupscalingEnabled(serviceGroupDefinition.isGroupScalingEnabled());
         List<String> subGroupsDef = serviceGroupDefinition.getSubGroups();
         List<String> cartridgesDef = serviceGroupDefinition.getCartridges();
