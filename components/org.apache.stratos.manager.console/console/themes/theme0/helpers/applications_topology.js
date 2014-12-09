3ec2cb86d87673ce2733d4e59c03723ada002a42
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-12-09 11:26:37 +0530

    Application Topology view added without refresh

---
+++ components/org.apache.stratos.manager.console/console/themes/theme0/helpers/applications_topology.js
@@ -0,0 +1,6 @@
+var resources = function (page, meta) {
+    return {
+        js: ['JSONEditor-0.7.12/jsoneditor-0.7.12.js','bootstrap-switch-3.0.2/bootstrap-switch.min.js', 'custom/script.js', 'd3js-v3/d3.v3.min.js' ],
+        css: ['bootstrap-switch-3.0.2/bootstrap-switch.min.css', 'custom/style.css', 'custom/topology.css']
+    };
+};
