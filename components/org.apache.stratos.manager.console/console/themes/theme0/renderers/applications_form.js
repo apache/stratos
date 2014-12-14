ddda83a9ca709a0a409a4c69a6064b2e1cfb7a80
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-12-14 18:02:28 +0530

    fix application  listing hbs issues after data return changes

--- components/org.apache.stratos.manager.console/console/themes/theme0/renderers/applications_form.js
+++ components/org.apache.stratos.manager.console/console/themes/theme0/renderers/applications_form.js
@@ -205,6 +205,7 @@ var render = function (theme, data, meta, require) {
                             formData: data.formData,
                             formDataRaw: data.formDataRaw,
                             formDataEdit: data.formDataEdit,
+                            formtype:data.formtype,
                             isForm: data.isForm,
                             isEdit:data.isEdit,
                             formTitle: data.formTitle,
