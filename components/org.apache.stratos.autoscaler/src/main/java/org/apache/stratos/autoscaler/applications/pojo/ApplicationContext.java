0a826755f715ab69f26f139ddf072a417b15b64c
Imesh Gunaratne <imesh@apache.org>
2014-12-16 00:08:44 +0530

    Adding name and description attributes to application definition

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/applications/pojo/ApplicationContext.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/applications/pojo/ApplicationContext.java
@@ -30,10 +30,12 @@ public class ApplicationContext implements Serializable {
     public static final String STATUS_CREATED = "Created";
     public static final String STATUS_DEPLOYED = "Deployed";
 
+    private String applicationId;
+    private String name;
+    private String description;
     private int tenantId;
     private String tenantDomain;
     private String teantAdminUsername;
-    private String applicationId;
     private String alias;
     private ComponentContext componentContext;
     private Properties properties;
@@ -105,5 +107,21 @@ public class ApplicationContext implements Serializable {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
