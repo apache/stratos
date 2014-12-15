11dacc7e57f9cff86b79e79ec3e1714bfedc693c
Lahiru Sandaruwan <lahirus@apache.org>
2014-12-15 09:26:45 +0530

    Adding ScalingOverMaxEvent

---
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/monitor/events/ScalingOverMaxEvent.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.stratos.autoscaler.monitor.events;
+
+/**
+ * This is to handle scaling of the monitors
+ */
+public class ScalingOverMaxEvent extends MonitorEvent {
+
+    private float factor;
+    private String networkPartitionId;
+    private String serviceName;
+
+    public ScalingOverMaxEvent(String id, String networkPartitionId, String instanceId, float factor, String serviceName) {
+        super(id, instanceId);
+        this.factor = factor;
+        this.networkPartitionId = networkPartitionId;
+        this.serviceName = serviceName;
+    }
+
+    public float getFactor() {
+        return factor;
+    }
+
+    public void setFactor(float factor) {
+        this.factor = factor;
+    }
+
+    public String getNetworkPartitionId() {
+        return networkPartitionId;
+    }
+
+    public void setNetworkPartitionId(String networkPartitionId) {
+        this.networkPartitionId = networkPartitionId;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+}
