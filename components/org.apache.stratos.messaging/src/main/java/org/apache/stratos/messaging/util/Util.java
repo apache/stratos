98e5efe7f0a05ff71f2546fda9b5a83a702fbf9a
gayan <gayan@puppet.gayan.org>
2014-12-19 15:56:59 +0530

    Update the messaging model util with apache commons isNumber util method

--- components/org.apache.stratos.messaging/src/main/java/org/apache/stratos/messaging/util/Util.java
+++ components/org.apache.stratos.messaging/src/main/java/org/apache/stratos/messaging/util/Util.java
@@ -23,7 +23,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.stratos.messaging.event.Event;
 import org.apache.stratos.messaging.message.JsonMessage;
-import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang.math.NumberUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
