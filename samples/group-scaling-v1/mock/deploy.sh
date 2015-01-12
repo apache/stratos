c282c20c21533d46475bb43e6dc46f2314dfd25c
Manula Thantriwatte <manula@apache.org>
2015-01-12 09:46:48 +0000

    Correct the resource path in sample JSON files

--- samples/group-scaling-v1/mock/deploy.sh
+++ samples/group-scaling-v1/mock/deploy.sh
@@ -27,5 +27,5 @@ curl -X POST -H "Content-Type: application/json" -d @'artifacts/composite_applic
 
 sleep 3
 # Deploy application
-curl -X POST -H "Content-Type: application/json" -d@'artifacts/app_deployment_policy.json' -k -v -u admin:admin https://localhost:9443/api/application/appscaling/deploy
+curl -X POST -H "Content-Type: application/json" -d@'artifacts/app_deployment_policy.json' -k -v -u admin:admin https://localhost:9443/api/applications/appscaling/deploy
 
