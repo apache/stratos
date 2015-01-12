6c4930d16bd82c4fa141f1b9fde22faa4d6827a4
gayan <gayan@puppet.gayan.org>
2015-01-12 16:58:34 +0530

    Update DEPENDENCY_CLUSTER_IDS to handle null values

--- components/org.apache.stratos.python.cartridge.agent/cartridgeagent/cartridgeagent/modules/config/cartridgeagentconfiguration.py
+++ components/org.apache.stratos.python.cartridge.agent/cartridgeagent/cartridgeagent/modules/config/cartridgeagentconfiguration.py
@@ -133,7 +133,7 @@ class CartridgeAgentConfiguration:
                 self.app_path = self.read_property(cartridgeagentconstants.APPLICATION_PATH, False)
                 self.repo_url = self.read_property(cartridgeagentconstants.REPO_URL, False)
                 self.ports = str(self.read_property(cartridgeagentconstants.PORTS)).split("|")
-                self.dependant_cluster_id = self.read_property(cartridgeagentconstants.DEPENDENCY_CLUSTER_IDS)
+                self.dependant_cluster_id = self.read_property(cartridgeagentconstants.DEPENDENCY_CLUSTER_IDS, False)
 
                 try:
                     self.log_file_paths = str(
