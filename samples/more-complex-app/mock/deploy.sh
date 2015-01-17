692f0167b03f9ac5fa1ba68039438f97969f8b2a
Imesh Gunaratne <imesh@apache.org>
2015-01-17 20:55:57 +0530

    Fixing sample application: more-complex-app artifacts

--- samples/more-complex-app/mock/deploy.sh
+++ samples/more-complex-app/mock/deploy.sh
@@ -1,44 +1,40 @@
 #!/bin/sh 
 
 # Create autoscale policy
-curl -X POST -H "Content-Type: application/json" -d @'autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
 
 # Create tomcat cartridge
-curl -X POST -H "Content-Type: application/json" -d @'tomcat.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/tomcat.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 
 # Deploy tomcat1 cartride
-curl -X POST -H "Content-Type: application/json" -d @'tomcat1.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/tomcat1.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 
 # Deploy tomcat2 cartride
-curl -X POST -H "Content-Type: application/json" -d @'tomcat2.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/tomcat2.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 
 # Deploy group
-curl -X POST -H "Content-Type: application/json" -d @'group6c.json' -k -v -u admin:admin https://localhost:9443/api/groups
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/group6c.json' -k -v -u admin:admin https://localhost:9443/api/groups
 
-curl -X POST -H "Content-Type: application/json" -d @'group8c.json' -k -v -u admin:admin https://localhost:9443/api/groups
-
-# GET group
-#curl -X GET -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/groups/group6
-
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/group8c.json' -k -v -u admin:admin https://localhost:9443/api/groups
 
 sleep 5
 # Create application
-curl -X POST -H "Content-Type: application/json" -d @'application_definition.json' -k -v -u admin:admin https://localhost:9443/api/applications
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/application_definition.json' -k -v -u admin:admin https://localhost:9443/api/applications
 
 sleep 5
 # GET application
-# curl -X GET -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applications/myapp1265
+curl -X GET -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applications/app_boo
 
 # Deploy application
-#curl -X POST -H "Content-Type: application/json" -d@'deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
+curl -X POST -H "Content-Type: application/json" -d@'artifacts/deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applications/app_boo/deploy
+
+sleep 20
 
 # Undeploy application
-#curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applicationDeployments/myapp1265
+# curl -X POST -H "Content-Type: application/json" -d@'artifacts/deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applications/app_boo/undeploy
+
+sleep 5
 
 # Delete application
-#curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applications/app_boo
-
-sleep 3
-#curl -X POST -H "Content-Type: application/json" -d@'grouping/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/application/app_boo/deploy
-curl -X POST -H "Content-Type: application/json" -d@'deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applications/app_boo/deploy
+# curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applications/app_boo
 
