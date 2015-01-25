958a3100a833e4de7f8fdb710099c44469853936
Imesh Gunaratne <imesh@apache.org>
2015-01-25 11:02:18 +0530

    Renaming org.apache.stratos.metadataservice to org.apache.stratos.metadata.service

---
+++ components/org.apache.stratos.metadata.service/src/main/java/org/apache/stratos/metadata/service/exception/MetadataException.java
@@ -0,0 +1,9 @@
+package org.apache.stratos.metadata.service.exception;
+
+
+public class MetadataException extends Exception {
+
+    public MetadataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- components/org.apache.stratos.metadataservice/src/main/java/org/apache/stratos/metadataservice/exception/MetadataException.java
+++
@@ -1,9 +0,0 @@
-package org.apache.stratos.metadataservice.exception;
-
-
-public class MetadataException extends Exception {
-
-    public MetadataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
