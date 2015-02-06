d4b502144ea80a71488040824a3c24b57e00db32
Imesh Gunaratne <imesh@apache.org>
2015-02-06 19:34:42 +0530

    Adding cartridge docker image build script

---
+++ tools/docker-images/cartridge-docker-images/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+# ---------------------------------------------------------------
+
+set -e
+
+prgdir=`dirname "$0"`
+script_path=`cd "$prgdir"; pwd`
+pca_source_path=`cd "$script_path/../../../components/org.apache.stratos.python.cartridge.agent/"; pwd`
+
+pushd ${pca_source_path}
+mvn clean install
+cp -vf target/apache-stratos-python-cartridge-agent-4.1.0-SNAPSHOT.zip ${script_path}/base-image/files/
+popd
+
+pushd ${script_path}/base-image/
+echo "Building base image..."
+docker build -t stratos/base-image:4.1.0-alpha .
+
+echo "Pushing base image to docker hub..."
+docker push stratos/base-image:4.1.0-alpha
+popd
+
+pushd ${script_path}/service-images/php
+echo "Building php image..."
+docker build -t stratos/php:4.1.0-alpha .
+
+echo "Pushing php image to docker hub..."
+docker push stratos/php:4.1.0-alpha
+popd
