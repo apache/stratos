cbea106b36d79f3017599c3d8492e47b9d1a68e8
Gayan Gunarathne <gayang@wso2.com>
2015-02-10 15:12:39 +0530

    Update the docker file scripts with base-image:rc1 version and latest agent zip file

--- tools/docker-images/cartridge-docker-images/build.sh
+++ tools/docker-images/cartridge-docker-images/build.sh
@@ -26,7 +26,7 @@ pca_source_path=`cd "$script_path/../../../components/org.apache.stratos.python.
 
 pushd ${pca_source_path}
 mvn clean install
-cp -vf target/apache-stratos-python-cartridge-agent-4.1.0-SNAPSHOT.zip ${script_path}/base-image/files/
+cp -vf target/apache-stratos-python-cartridge-agent-4.1.0-SNAPSHOT.zip ${script_path}/base-image/packs/
 popd
 
 pushd ${script_path}/base-image/
