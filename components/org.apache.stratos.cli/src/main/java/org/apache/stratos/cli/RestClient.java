d3edd8fa784b40d8e3850acc81718b4a4bfb5a48
Dinithi <dinithis@wso2.com>
2015-02-18 12:07:04 +0530

    response code checking condition changed for undeployEntity method

--- components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/RestClient.java
+++ components/org.apache.stratos.cli/src/main/java/org/apache/stratos/cli/RestClient.java
@@ -172,7 +172,7 @@ public class RestClient implements GenericRestClient {
             int responseCode = executeDelete(serviceEndpoint, entityId);
             if (responseCode == 404) {
                 System.out.println(String.format("%s not found", StringUtils.capitalize(entityName)));
-            } else if (responseCode == 204) {
+            } else if (responseCode >= 200 && responseCode <300) {
                 System.out.println(String.format("Successfully un-deployed %s", entityName));
             }
         } catch (Exception e) {
