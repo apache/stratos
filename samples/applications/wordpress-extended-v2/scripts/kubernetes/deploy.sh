36f0d3a6f2848be360c22589a518eb8112941f49
R-Rajkumar <rraju1990@gmail.com>
2015-02-26 20:17:43 +0530

    re-arranging samples to new format

--- samples/applications/wordpress-extended-v2/scripts/kubernetes/deploy.sh
+++ samples/applications/wordpress-extended-v2/scripts/kubernetes/deploy.sh
@@ -8,8 +8,9 @@ prgdir=`dirname "$0"`
 script_path=`cd "$prgdir"; pwd`
 common_folder=`cd "${script_path}/../common"; pwd`
 iaas_artifacts_path=`cd "${script_path}/../../artifacts/${iaas}"; pwd`
+kubernetes_clusters_path=`cd "${script_path}/../../../../kubernets-clusters"; pwd`
 
 echo "Adding kubernetes cluster..."
-curl -X POST -H "Content-Type: application/json" -d "@${iaas_artifacts_path}/kubernetes-cluster.json" -k -u admin:admin https://${host_ip}:${host_port}/api/kubernetesClusters
+curl -X POST -H "Content-Type: application/json" -d "@${kubernetes_clusters_path}/kubernetes-cluster-1.json" -k -u admin:admin https://${host_ip}:${host_port}/api/kubernetesClusters
 
 bash ${common_folder}/deploy.sh ${iaas}
