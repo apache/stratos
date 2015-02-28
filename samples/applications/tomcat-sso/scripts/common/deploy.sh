c9f1ad8f873e8610ebe24a80391c3415680fe3f4
Imesh Gunaratne <imesh@apache.org>
2015-03-01 01:53:12 +0530

    Fixing deployment policy in tomcat-sso sample application

--- samples/applications/tomcat-sso/scripts/common/deploy.sh
+++ samples/applications/tomcat-sso/scripts/common/deploy.sh
@@ -27,10 +27,10 @@ echo "Adding autoscale policy..."
 curl -X POST -H "Content-Type: application/json" -d "@${autoscaling_policies_path}/autoscaling-policy-1.json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/autoscalingPolicies
 
 echo "Adding network partitions..."
-curl -X POST -H "Content-Type: application/json" -d "@${network_partitions_path}/network-partition-1.json" -k -v -u admin:admin https://${host_ip}:9443/api/networkPartitions
+curl -X POST -H "Content-Type: application/json" -d "@${network_partitions_path}/network-partition-3.json" -k -v -u admin:admin https://${host_ip}:9443/api/networkPartitions
 
 echo "Adding deployment policy..."
-curl -X POST -H "Content-Type: application/json" -d "@${deployment_policies_path}/deployment-policy-1.json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/deploymentPolicies
+curl -X POST -H "Content-Type: application/json" -d "@${deployment_policies_path}/deployment-policy-3.json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/deploymentPolicies
 
 echo "Adding tomcat cartridge..."
 curl -X POST -H "Content-Type: application/json" -d "@${iaas_cartridges_path}/tomcat-sso.json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/cartridges
