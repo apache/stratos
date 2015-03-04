c58be4d8816dbb748026e25938c15f407aad9b0c
R-Rajkumar <rraju1990@gmail.com>
2015-03-04 20:40:39 +0530

    updating single-group-v1 sample

--- samples/applications/single-group-v1/scripts/common/undeploy.sh
+++ samples/applications/single-group-v1/scripts/common/undeploy.sh
@@ -29,3 +29,7 @@ curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://
 echo "Removing network partitions..."
 curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/networkPartitions/network-partition-3
 
+echo "Removing application policies..."
+curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/applicationPolicies/application-policy-1
+
+
