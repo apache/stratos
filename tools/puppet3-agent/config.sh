7e6b3365d74be78f5a1b32bac52850cdd51c0d70
anuruddhal <anuruddha789@gmail.com>
2015-03-11 17:12:07 +0530

    Checking for existence of lock file before removing

--- tools/puppet3-agent/config.sh
+++ tools/puppet3-agent/config.sh
@@ -33,6 +33,7 @@ CP=`which cp`
 MV=`which mv`
 
 HOSTSFILE=/etc/hosts
+LOCKFILE=/mnt/apache-stratos-cartridge-agent-4.0.0/wso2carbon.lck
 DATE=`date +%d%m%y%S`
 RANDOMNUMBER="`${TR} -c -d 0-9 < /dev/urandom | ${HEAD} -c 4`${DATE}"
 
@@ -88,7 +89,11 @@ if [[ $answer = y ]] ; then
 	ARGS=("-n${NODEID}" "-d${DOMAIN}" "-s${PUPPET_IP}")
 	${ECHO} "\nRunning puppet installation with arguments: ${ARGS[@]}"
 	/root/bin/puppetinstall/puppetinstall "${ARGS[@]}"
-        ${RM} /mnt/apache-stratos-cartridge-agent-4.0.0/wso2carbon.lck
+	#check for java agent lock file
+	if [ -e ${LOCKFILE} ]; then
+    ${RM} ${LOCKFILE}
+	fi
+
 	${GREP} -q '/root/bin/init.sh > /tmp/puppet_log' /etc/rc.local || ${SED} -i 's/exit 0$/\/root\/bin\/init.sh \> \/tmp\/puppet_log\nexit 0/' /etc/rc.local
 	${RM} -rf /tmp/*
 	${RM} -rf /var/lib/puppet/ssl/*
