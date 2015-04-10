97c1b5e6a9dde861c9ec9feb7326b1500e068d28
anuruddhal <anuruddha789@gmail.com>
2015-04-10 16:10:32 +0530

    Fixing the UI topology view

--- components/org.apache.stratos.manager.console/console/themes/theme0/js/custom/applications_topology.js
+++ components/org.apache.stratos.manager.console/console/themes/theme0/js/custom/applications_topology.js
@@ -190,7 +190,7 @@ function update(source) {
                             "<strong>Status: </strong>" + d.status;
 
             } else if (d.type == 'members') {
-                if((d.ports != '') && (d.ports.length > 0)) {
+                if((typeof d.ports != 'undefined') && (d.ports.length > 0)) {
                     var portsHTML = "<strong>Ports: </strong></br>";
                     for(var i=0;i<d.ports.length;i++){
                         portsHTML += "Port: " + d.ports[i].port + ", Protocol: " + d.ports[i].protocol;
