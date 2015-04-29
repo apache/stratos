2fb25df936b633ba56d1f7ceb21284a1a86c6eec
Lahiru Sandaruwan <lahirus@apache.org>
2015-04-29 15:14:37 +0530

    Adding new exceptions to handle specific errors in rest API

---
+++ components/org.apache.stratos.rest.endpoint/src/main/java/org/apache/stratos/rest/endpoint/exception/InvalidDomainException.java
@@ -0,0 +1,10 @@
+package org.apache.stratos.rest.endpoint.exception;
+
+/**
+ * Created by lahiru on 4/28/15.
+ */
+public class InvalidDomainException extends RestAPIException{
+    public InvalidDomainException(String msg) {
+        super(msg);
+    }
+}
