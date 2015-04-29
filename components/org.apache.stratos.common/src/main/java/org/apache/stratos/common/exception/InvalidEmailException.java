833f7199c8e44e7a1a068c6c711dc6cd56ce6b72
Lahiru Sandaruwan <lahirus@apache.org>
2015-04-29 15:11:38 +0530

    Adding InvalidEmailException to handle specific error, of invalid email

---
+++ components/org.apache.stratos.common/src/main/java/org/apache/stratos/common/exception/InvalidEmailException.java
@@ -0,0 +1,9 @@
+package org.apache.stratos.common.exception;
+
+/**
+ * Created by lahiru on 4/28/15.
+ */
+public class InvalidEmailException extends Throwable {
+    public InvalidEmailException(String msg) {
+    }
+}
