4fb23bfac719fb762f5494381f8d4e72fbd749b6
reka <rthirunavukkarasu23@gmail.com>
2015-04-30 17:54:47 +0530

    updated undeploy script for dependency-scaling-cartridges-app

--- samples/applications/scaling/dependent-scaling/sample-cartridges/scripts/common/undeploy.sh
+++ samples/applications/scaling/dependent-scaling/sample-cartridges/scripts/common/undeploy.sh
@@ -6,12 +6,12 @@ host_port=9443
 set -e
 
 echo "Undeploying application..."
-curl -X POST -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/applications/sample-cartridges-app/undeploy
+curl -X POST -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/applications/dependency-scaling-cartridges-app/undeploy
 
 sleep 10
 
 echo "Deleting application..."
-curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/applications/sample-cartridges-app
+curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/applications/dependency-scaling-cartridges-app
 
 echo "Removing cartridges..."
 curl -X DELETE -H "Content-Type: application/json" -k -v -u admin:admin https://${host_ip}:${host_port}/api/cartridges/tomcat
