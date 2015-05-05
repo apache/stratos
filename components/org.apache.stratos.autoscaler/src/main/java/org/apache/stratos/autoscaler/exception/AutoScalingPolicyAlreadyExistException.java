dff5eb1ba84dc2a4b9d2a1340c1bf75eb44d7ab6
Lahiru Sandaruwan <lahirus@apache.org>
2015-05-05 12:14:14 +0530

    Update exceptions to throw specific errors

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/exception/AutoScalingPolicyAlreadyExistException.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/exception/AutoScalingPolicyAlreadyExistException.java
@@ -1,6 +1,6 @@
 package org.apache.stratos.autoscaler.exception;
 
-public class AutoScalingPolicyAlreadyExistException extends AutoScalerException {
+public class AutoScalingPolicyAlreadyExistException extends Exception {
     public AutoScalingPolicyAlreadyExistException(String msg) {
         super(msg);
     }
---
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/exception/CartridgeGroupNotFoundException.java
@@ -0,0 +1,7 @@
+package org.apache.stratos.autoscaler.exception;
+
+public class CartridgeGroupNotFoundException extends AutoScalerException {
+    public CartridgeGroupNotFoundException(String msg) {
+        super(msg);
+    }
+}
