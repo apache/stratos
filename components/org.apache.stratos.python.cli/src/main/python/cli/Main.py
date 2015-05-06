2d70b5376594d4365120662374866115d4d3438b
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-05-06 12:03:25 +0530

    Initializing the CMD and started the loop in Main On branch python-cli modified: components/org.apache.stratos.python.cli/src/main/python/cli/Main.py
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Main.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Main.py
@@ -1,8 +1,13 @@
-def main():
-	pass
+import sys
+import CLI
 
+
+def main():
+    cli = CLI()
+    if len(sys.argv) > 1:
+        cli.onecmd(' '.join(sys.argv[1:]))
+    else:
+        cli.cmdloop()
 
 if __name__ == '__main__':
-	main()
-	
-	
+    main()
