b33aee677daffcd4529a4fcae148af7e5f581d9e
Pubudu Gunatilaka <pubudu538@gmail.com>
2015-05-13 18:22:20 +0530

    Adding missing license headers in files

--- components/org.apache.stratos.manager.console/console/themes/theme0/js/jquery.contextMenu/jquery.ui.position.js
+++ components/org.apache.stratos.manager.console/console/themes/theme0/js/jquery.contextMenu/jquery.ui.position.js
@@ -1,13 +1,24 @@
-/*!
- * jQuery UI Position v1.10.0
- * http://jqueryui.com
+/*
  *
- * Copyright 2013 jQuery Foundation and other contributors
- * Released under the MIT license.
- * http://jquery.org/license
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://api.jqueryui.com/position/
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  */
+ 
 (function( $, undefined ) {
 
 $.ui = $.ui || {};
