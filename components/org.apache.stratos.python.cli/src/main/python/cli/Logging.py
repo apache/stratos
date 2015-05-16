2977d82ce6b61f4bbe0227530626b25f58281702
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-05-16 11:29:42 +0530

    Creating stratos folder for logging( if not exists ), logging in to stratos-cli.log
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Logging.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Logging.py
@@ -1,4 +1,16 @@
 import logging
+import os
+
+stratos_dir = os.path.expanduser('~/.stratos')
+log_file_path = stratos_dir+"stratos-cli.log"
 
 
-logging.basicConfig(filename="~/debug.log", level=logging.DEBUG)
+if not os.path.exists(stratos_dir):
+    try:
+        os.makedirs(stratos_dir)
+        logging.info("Created directory: "+stratos_dir)
+    except OSError:
+        logging.warning("Failed to create directory: "+stratos_dir)
+
+
+logging.basicConfig(filename=log_file_path, level=logging.DEBUG)
