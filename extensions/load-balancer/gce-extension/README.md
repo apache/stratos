dfe2e2c4f56f3b4a297b107a6e60525d24c78459
asanka sanjaya <angal.sh@gmail.com>
2015-06-03 10:07:29 +0000

    Adding initial files for gce load balancer extension

---
+++ extensions/load-balancer/gce-extension/README.md
@@ -0,0 +1,6 @@
+# Apache Stratos GCE Extension
+
+Apache Stratos GCE extension is a load balancer extension for Google Compute Engine Load Balancer. It is an executable program
+which can manage the life-cycle of a GCE load balancer instance according to the topology, composite application model,
+tenant application signups and domain mapping information received from Stratos via the message broker.
+
---
+++ extensions/load-balancer/gce-extension/src/main/bin/gce-extension.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# --------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# --------------------------------------------------------------
+
+echo "Starting gce load balancer extension..."
+script_path="$( cd -P "$( dirname "$SOURCE" )" && pwd )/`dirname $0`"
+echo ${script_path}
+lib_path=${script_path}/../lib/
+class_path=`echo ${lib_path}/*.jar | tr ' ' ':'`
+
+
+
+properties="-Djndi.properties.dir=${script_path}/../conf
+            -Dstats.socket.file.path=/tmp/haproxy-stats.socket
+            -Dlog4j.properties.file.path=${script_path}/../conf/log4j.properties
+            -Djavax.net.ssl.trustStore=${script_path}/../security/client-truststore.jks
+            -Djavax.net.ssl.trustStorePassword=wso2carbon
+            -Dthrift.client.config.file.path=${script_path}/../conf/thrift-client-config.xml
+            -Dcep.stats.publisher.enabled=false
+            -Dthrift.receiver.ip=127.0.0.1
+            -Dthrift.receiver.port=7615
+            -Dnetwork.partition.id=network-partition-1"
+
+
+# Uncomment below line to enable remote debugging
+debug="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5006"
+
+java -cp "${class_path}" ${properties} ${debug} org.apache.stratos.gce.extension.Main $*
