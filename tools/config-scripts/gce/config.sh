57e1dcb5449992c42c5d337fc863c92f7f3c7edc
asanka sanjaya <angal.sh@gmail.com>
2015-06-04 06:06:19 +0000

    reformat the code in gce config.sh

--- tools/config-scripts/gce/config.sh
+++ tools/config-scripts/gce/config.sh
@@ -92,7 +92,7 @@ if [[ $answer = y ]] ; then
 	${ECHO} "\nRunning puppet installation with arguments: ${ARGS[@]}"
 	/root/bin/puppetinstall/puppetinstall "${ARGS[@]}"
 	if [[ -e ${LOCKFILE} ]]; then
-		${RM} ${LOCKFILE}
+	    ${RM} ${LOCKFILE}
 	fi
 
 	${GREP} -q '/root/bin/init.sh > /tmp/puppet_log' /etc/rc.local || ${SED} -i 's/exit 0$/\/root\/bin\/init.sh \> \/tmp\/puppet_log\nexit 0/' /etc/rc.local
