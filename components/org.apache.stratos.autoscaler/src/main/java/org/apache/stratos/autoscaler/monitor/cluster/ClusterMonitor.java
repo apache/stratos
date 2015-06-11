af6ade4501f3d8272704a617376e0c218baa0d23
Lahiru Sandaruwan <lahirus@apache.org>
2015-06-11 15:40:31 +0530

    Fix member stat context removal on member maintenance event

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/monitor/cluster/ClusterMonitor.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/monitor/cluster/ClusterMonitor.java
@@ -1157,12 +1157,16 @@ public class ClusterMonitor extends Monitor {
                 clusterInstanceId);
         ClusterLevelPartitionContext clusterMonitorPartitionContext = networkPartitionCtxt.
                 getPartitionCtxt(partitionId);
-        clusterMonitorPartitionContext.addMemberStatsContext(new MemberStatsContext(memberId));
+
+        clusterMonitorPartitionContext.moveActiveMemberToTerminationPendingMembers(memberId);
+        if (clusterMonitorPartitionContext.getMemberStatsContext(memberId) != null) {
+            clusterMonitorPartitionContext.removeMemberStatsContext(memberId);
+        }
         if (log.isDebugEnabled()) {
-            log.debug(String.format("Member has been moved as pending termination: "
+            log.debug(String.format("Member has been moved as pending termination, and member stat context is removed: "
                     + "[member] %s", memberId));
         }
-        clusterMonitorPartitionContext.moveActiveMemberToTerminationPendingMembers(memberId);
+
     }
 
     public void handleMemberReadyToShutdownEvent(MemberReadyToShutdownEvent memberReadyToShutdownEvent) {
@@ -1467,7 +1471,7 @@ public class ClusterMonitor extends Monitor {
             } else {
                 createClusterInstance(cluster.getServiceName(), cluster.getClusterId(), null, parentInstanceId, partitionId,
                         parentMonitorInstance.getNetworkPartitionId());
-                if(log.isDebugEnabled()) {
+                if (log.isDebugEnabled()) {
                     log.debug(String.format("Cluster instance created: [application-id] %s [service-name] %s " +
                             "[cluster-id] %s", appId, cluster.getServiceName(), cluster.getClusterId()));
                 }
