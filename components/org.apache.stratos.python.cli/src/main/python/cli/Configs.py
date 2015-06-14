f49bebd454326c7d152ac160ab760c2088dfbc27
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-06-14 13:15:33 +0530

    Some quick fixes Using python os.path.join() to avoid forward/back slash problems Setting empty strings as defaults for username and password evn vars instead of None
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
@@ -6,8 +6,8 @@ stratos_dir = "~/.stratos"
 log_file_name = "stratos-cli.log"
 
 stratos_dir_path = os.path.expanduser(stratos_dir)
-log_file_path = stratos_dir_path+log_file_name
+log_file_path = os.path.join(stratos_dir_path, log_file_name)
 
-Stratos_url = os.getenv('STRATOS_URL', None)
-Stratos_username = os.getenv('STRATOS_USERNAME', None)
-Stratos_password = os.getenv('STRATOS_PASSWORD', None)
+stratos_url = os.getenv('STRATOS_URL', None)
+stratos_username = os.getenv('STRATOS_USERNAME', "")
+stratos_password = os.getenv('STRATOS_PASSWORD', "")
