78d69ccdafb83ec18b9a899603d7cd7dbb59be6a
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-06-18 07:10:06 +0530

    Default stratos URL added
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
@@ -8,6 +8,6 @@ log_file_name = "stratos-cli.log"
 stratos_dir_path = os.path.expanduser(stratos_dir)
 log_file_path = os.path.join(stratos_dir_path, log_file_name)
 
-stratos_url = os.getenv('STRATOS_URL', None)
+stratos_url = os.getenv('STRATOS_URL', "https://localhost:9443/")
 stratos_username = os.getenv('STRATOS_USERNAME', "")
 stratos_password = os.getenv('STRATOS_PASSWORD', "")
