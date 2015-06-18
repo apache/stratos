f7a047d3389780947704facfec85eb0ea25e723c
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-06-18 22:11:09 +0530

    Stratos API URL added
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Configs.py
@@ -9,5 +9,6 @@ stratos_dir_path = os.path.expanduser(stratos_dir)
 log_file_path = os.path.join(stratos_dir_path, log_file_name)
 
 stratos_url = os.getenv('STRATOS_URL', "https://localhost:9443/")
+stratos_api_url = stratos_url + "api/"
 stratos_username = os.getenv('STRATOS_USERNAME', "")
 stratos_password = os.getenv('STRATOS_PASSWORD', "")
