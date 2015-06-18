26b547069268f0605d8dd49c9eff71883b37a028
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-06-18 22:09:09 +0530

    Prompting for user credentials when starting the interactive mode
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Main.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Main.py
@@ -1,6 +1,24 @@
 import sys
+import getpass
 from CLI import CLI
-import Logging
+import Configs
+
+# Fix Python 2.x.
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+def prompt_for_credentials():
+    """Prompt for user credentials"""
+    while True:
+        _username = input("Username: ")
+        _password = getpass.getpass("Password: ")
+        if _username is not "" and _password is not "":
+            Configs.stratos_username = _username
+            Configs.stratos_password = _password
+            break
 
 
 def main():
@@ -9,6 +27,7 @@ def main():
     if len(sys.argv) > 1:
         cli.onecmd(' '.join(sys.argv[1:]))
     else:
+        prompt_for_credentials()
         cli.cmdloop()
 
 if __name__ == '__main__':
