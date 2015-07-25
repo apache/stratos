f4ee826aa2206d5853ad1614a1ef1825ec7475bb
Akila Perera <ravihansa3000@gmail.com>
2015-07-25 21:35:05 +0530

    Prepare for next development iteration: update poms to 4.1.1-SNAPSHOT

--- products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/StratosTestServerManager.java
+++ products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/StratosTestServerManager.java
@@ -47,7 +47,7 @@ public class StratosTestServerManager extends TestServerManager {
     private static final Log log = LogFactory.getLog(StratosTestServerManager.class);
 
     private final static String CARBON_ZIP = SampleApplicationsTest.class.getResource("/").getPath() +
-            "/../../../distribution/target/apache-stratos-4.1.0.zip";
+            "/../../../distribution/target/apache-stratos-4.1.1-SNAPSHOT.zip";
     private final static int PORT_OFFSET = 0;
     private static final String ACTIVEMQ_BIND_ADDRESS = "tcp://localhost:61617";
     private static final String MOCK_IAAS_XML_FILE = "mock-iaas.xml";
