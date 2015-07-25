7e05c887aa329d169f3415418e8e4f9cbdabdd93
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-07-25 12:31:14 +0530

    added list_users to Straos
    Signed-off-by: Imesh Gunaratne <imesh@apache.org>

--- components/org.apache.stratos.python.cli/src/main/python/cli/Stratos.py
+++ components/org.apache.stratos.python.cli/src/main/python/cli/Stratos.py
@@ -1,4 +1,5 @@
 import requests
+import Configs
 
 
 class Stratos:
@@ -6,5 +7,20 @@ class Stratos:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def list_users():
+        r = requests.get(Configs.stratos_url + 'users',
+                         auth=(Configs.stratos_username, Configs.stratos_password), verify=False)
+        if r.status_code is 200:
+            return r.json()
+
+        elif r.status_code is 400:
+            raise requests.HTTPError()
+
+    @staticmethod
+    def network_partitions():
+        pass
+
 
 
