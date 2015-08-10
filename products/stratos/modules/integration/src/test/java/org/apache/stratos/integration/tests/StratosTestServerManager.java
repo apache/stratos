4b96ba010f659685a64a8ba8e64b910b57e09086
Gayan Gunarathne <gayang@wso2.com>
2015-08-10 16:16:14 +0530

    Add the updated integration tests

--- products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/StratosTestServerManager.java
+++ products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/StratosTestServerManager.java
@@ -26,6 +26,8 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.stratos.common.test.TestLogAppender;
+import org.apache.stratos.integration.tests.application.SampleApplicationsTest;
+import org.apache.stratos.integration.tests.rest.RestClient;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.wso2.carbon.integration.framework.TestServerManager;
@@ -53,20 +55,23 @@ public class StratosTestServerManager extends TestServerManager {
     private static final String MOCK_IAAS_XML_FILE = "mock-iaas.xml";
     private static final String JNDI_PROPERTIES_FILE = "jndi.properties";
     private static final String JMS_OUTPUT_ADAPTER_FILE = "JMSOutputAdaptor.xml";
+    protected RestClient restClient;
+    private String endpoint = "https://localhost:9443";
 
+    private BrokerService broker = new BrokerService();
+    private TestLogAppender testLogAppender = new TestLogAppender();
     private ServerUtils serverUtils;
     private String carbonHome;
 
     public StratosTestServerManager() {
         super(CARBON_ZIP, PORT_OFFSET);
         serverUtils = new ServerUtils();
+        restClient = new RestClient(endpoint, "admin", "admin");
     }
 
     @Override
     @BeforeSuite(timeOut = 600000)
     public String startServer() throws IOException {
-
-        TestLogAppender testLogAppender = new TestLogAppender();
         Logger.getRootLogger().addAppender(testLogAppender);
         Logger.getRootLogger().setLevel(Level.INFO);
 
@@ -74,13 +79,15 @@ public class StratosTestServerManager extends TestServerManager {
             // Start ActiveMQ
             long time1 = System.currentTimeMillis();
             log.info("Starting ActiveMQ...");
-            BrokerService broker = new BrokerService();
+            broker.setDataDirectory(StratosTestServerManager.class.getResource("/").getPath() +
+                    File.separator + ".." + File.separator + "activemq-data");
             broker.setBrokerName("testBroker");
             broker.addConnector(ACTIVEMQ_BIND_ADDRESS);
             broker.start();
             long time2 = System.currentTimeMillis();
-            log.info(String.format("ActiveMQ started in %d sec", (time2 - time1)/1000));
-        } catch (Exception e) {
+            log.info(String.format("ActiveMQ started in %d sec", (time2 - time1) / 1000));
+        }
+        catch (Exception e) {
             throw new RuntimeException("Could not start ActiveMQ", e);
         }
 
@@ -104,24 +111,48 @@ public class StratosTestServerManager extends TestServerManager {
                 this.serverUtils.startServerUsingCarbonHome(carbonHome, carbonHome, "stratos", PORT_OFFSET, null);
                 FrameworkSettings.init();
 
-                while (!serverStarted(testLogAppender)) {
+                while (!serverStarted()) {
                     log.info("Waiting for topology to be initialized...");
                     Thread.sleep(5000);
                 }
 
+                while (!mockServiceStarted()) {
+                    log.info("Waiting for mock service to be initialized...");
+                    Thread.sleep(1000);
+                }
+
                 long time4 = System.currentTimeMillis();
-                log.info(String.format("Stratos server started in %d sec", (time4 - time3)/1000));
+                log.info(String.format("Stratos server started in %d sec", (time4 - time3) / 1000));
                 return carbonHome;
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             throw new RuntimeException("Could not start Stratos server", e);
         }
+    }
+
+    private boolean mockServiceStarted() {
+        for (String message : testLogAppender.getMessages()) {
+            if (message.contains("Mock IaaS service component activated")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     @AfterSuite(timeOut = 600000)
     public void stopServer() throws Exception {
         super.stopServer();
+       /*
+       while (!serverStopped()) {
+            log.info("Waiting for server to be shutdown...");
+            Thread.sleep(1000);
+        }
+        log.info("Stopped Apache Stratos server.");
+        */
+        broker.stop();
+        log.info("Stopped ActiveMQ server.");
     }
 
     protected void copyArtifacts(String carbonHome) throws IOException {
@@ -144,9 +175,19 @@ public class StratosTestServerManager extends TestServerManager {
         log.info(sourceFilePath + " file copied");
     }
 
-    private boolean serverStarted(TestLogAppender testLogAppender) {
-        for(String message : testLogAppender.getMessages()) {
-            if(message.contains("Topology initialized")) {
+    private boolean serverStopped() {
+        for (String message : testLogAppender.getMessages()) {
+            if (message.contains("Halting JVM")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private boolean serverStarted() {
+        for (String message : testLogAppender.getMessages()) {
+            if (message.contains("Topology initialized")) {
                 return true;
             }
         }
--- products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/rest/RestClient.java
+++ products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/rest/RestClient.java
@@ -19,19 +19,32 @@
 package org.apache.stratos.integration.tests.rest;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URI;
 
 /**
  * Rest client to handle rest requests
  */
 public class RestClient {
-
+    private static final Log log = LogFactory.getLog(RestClient.class);
     private DefaultHttpClient httpClient;
+    private String endPoint;
+    private String userName;
+    private String password;
 
     public RestClient() {
         PoolingClientConnectionManager cm = new PoolingClientConnectionManager();
@@ -44,17 +57,22 @@ public class RestClient {
         httpClient = (DefaultHttpClient) WebClientWrapper.wrapClient(httpClient);
     }
 
+    public RestClient(String endPoint, String userName, String password) {
+        this();
+        this.endPoint = endPoint;
+        this.userName = userName;
+        this.password = password;
+    }
+
     /**
      * Handle http post request. Return String
      *
      * @param resourcePath    This should be REST endpoint
      * @param jsonParamString The json string which should be executed from the post request
-     * @param userName
-     * @param password
      * @return The HttpResponse
      * @throws Exception if any errors occur when executing the request
      */
-    public HttpResponse doPost(URI resourcePath, String jsonParamString, String userName, String password) throws Exception {
+    public HttpResponse doPost(URI resourcePath, String jsonParamString) throws Exception {
         HttpPost postRequest = null;
         try {
             postRequest = new HttpPost(resourcePath);
@@ -62,7 +80,7 @@ public class RestClient {
             input.setContentType("application/json");
             postRequest.setEntity(input);
 
-            String userPass = userName + ":" + password;
+            String userPass = getUsernamePassword();
             String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userPass.getBytes("UTF-8"));
             postRequest.addHeader("Authorization", basicAuth);
 
@@ -76,18 +94,16 @@ public class RestClient {
      * Handle http get request. Return String
      *
      * @param resourcePath This should be REST endpoint
-     * @param userName
-     * @param password
      * @return The HttpResponse
      * @throws org.apache.http.client.ClientProtocolException and IOException
      *                                                        if any errors occur when executing the request
      */
-    public HttpResponse doGet(URI resourcePath, String userName, String password) throws Exception {
+    public HttpResponse doGet(URI resourcePath) throws Exception {
         HttpGet getRequest = null;
         try {
             getRequest = new HttpGet(resourcePath);
             getRequest.addHeader("Content-Type", "application/json");
-            String userPass = userName + ":" + password;
+            String userPass = getUsernamePassword();
             String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userPass.getBytes("UTF-8"));
             getRequest.addHeader("Authorization", basicAuth);
 
@@ -97,12 +113,12 @@ public class RestClient {
         }
     }
 
-    public HttpResponse doDelete(URI resourcePath, String userName, String password) throws Exception {
+    public HttpResponse doDelete(URI resourcePath) throws Exception {
         HttpDelete httpDelete = null;
         try {
             httpDelete = new HttpDelete(resourcePath);
             httpDelete.addHeader("Content-Type", "application/json");
-            String userPass = userName + ":" + password;
+            String userPass = getUsernamePassword();
             String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userPass.getBytes("UTF-8"));
             httpDelete.addHeader("Authorization", basicAuth);
             return httpClient.execute(httpDelete, new HttpResponseHandler());
@@ -111,7 +127,7 @@ public class RestClient {
         }
     }
 
-    public HttpResponse doPut(URI resourcePath, String jsonParamString, String userName, String password) throws Exception {
+    public HttpResponse doPut(URI resourcePath, String jsonParamString) throws Exception {
 
         HttpPut putRequest = null;
         try {
@@ -120,7 +136,7 @@ public class RestClient {
             StringEntity input = new StringEntity(jsonParamString);
             input.setContentType("application/json");
             putRequest.setEntity(input);
-            String userPass = userName + ":" + password;
+            String userPass = getUsernamePassword();
             String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userPass.getBytes("UTF-8"));
             putRequest.addHeader("Authorization", basicAuth);
             return httpClient.execute(putRequest, new HttpResponseHandler());
@@ -133,5 +149,209 @@ public class RestClient {
         if (request != null) {
             request.releaseConnection();
         }
+    }
+
+    public boolean addEntity(String filePath, String resourcePath, String entityName) {
+        try {
+            String content = getJsonStringFromFile(filePath);
+            URI uri = new URIBuilder(this.endPoint + resourcePath).build();
+
+            HttpResponse response = doPost(uri, content);
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return true;
+                } else {
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(), ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while trying to add ";
+            log.error(msg + entityName);
+            throw new RuntimeException(msg + entityName);
+        } catch (Exception e) {
+            String message = "Could not add " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    public boolean deployEntity(String resourcePath, String entityName) {
+        try {
+            URI uri = new URIBuilder(this.endPoint + resourcePath).build();
+
+            HttpResponse response = doPost(uri, "");
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return true;
+                } else {
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(), ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while trying to deploy ";
+            log.error(msg + entityName);
+            throw new RuntimeException(msg + entityName);
+        } catch (Exception e) {
+            String message = "Could not deploy  " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    public boolean undeployEntity(String resourcePath, String entityName) {
+        try {
+            URI uri = new URIBuilder(this.endPoint + resourcePath).build();
+
+            HttpResponse response = doPost(uri, "");
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return true;
+                } else {
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(), ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while trying to undeploy ";
+            log.error(msg + entityName);
+            throw new RuntimeException(msg + entityName);
+        } catch (Exception e) {
+            String message = "Could not deploy  " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    public Object getEntity(String resourcePath, String identifier, Class responseJsonClass,
+                            String entityName) {
+        try {
+            URI uri = new URIBuilder(this.endPoint + resourcePath + "/" + identifier).build();
+            HttpResponse response = doGet(uri);
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            Gson gson = gsonBuilder.create();
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return gson.fromJson(response.getContent(), responseJsonClass);
+                } else if (response.getStatusCode() == 404) {
+                    return null;
+                } else {
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(),
+                            ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while getting the " + entityName;
+            log.error(msg);
+            throw new RuntimeException(msg);
+        } catch (Exception e) {
+            String message = "Could not get " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    public boolean removeEntity(String resourcePath, String identifier, String entityName) {
+        try {
+            URI uri = new URIBuilder(this.endPoint + "/" + resourcePath + "/" + identifier).build();
+            HttpResponse response = doDelete(uri);
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return true;
+                } else if (response.getContent().contains("it is used") || response.getContent().contains("in use")) {
+                    return false;
+                } else {
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(),
+                            ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while removing the " + entityName;
+            log.error(msg);
+            throw new RuntimeException(msg);
+        } catch (Exception e) {
+            String message = "Could not remove  " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    public boolean updateEntity(String filePath, String resourcePath, String entityName) {
+        try {
+            String content = getJsonStringFromFile(filePath);
+            URI uri = new URIBuilder(this.endPoint + resourcePath).build();
+
+            HttpResponse response = doPut(uri, content);
+            if (response != null) {
+                if ((response.getStatusCode() >= 200) && (response.getStatusCode() < 300)) {
+                    return true;
+                } else {
+                    GsonBuilder gsonBuilder = new GsonBuilder();
+                    Gson gson = gsonBuilder.create();
+                    ErrorResponse errorResponse = gson.fromJson(response.getContent(),
+                            ErrorResponse.class);
+                    if (errorResponse != null) {
+                        throw new RuntimeException(errorResponse.getErrorMessage());
+                    }
+                }
+            }
+            String msg = "An unknown error occurred while trying to update ";
+            log.error(msg + entityName);
+            throw new RuntimeException(msg + entityName);
+        } catch (Exception e) {
+            String message = "Could not update " + entityName;
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    /**
+     * Get the json string from the artifacts directory
+     *
+     * @param filePath path of the artifacts
+     * @return json string of the relevant artifact
+     * @throws FileNotFoundException
+     */
+    public String getJsonStringFromFile(String filePath) throws FileNotFoundException {
+        JsonParser parser = new JsonParser();
+        Object object = parser.parse(new FileReader(getResourcesFolderPath() + filePath));
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+        return gson.toJson(object);
+    }
+
+    /**
+     * Get resources folder path
+     *
+     * @return the resource path
+     */
+    private String getResourcesFolderPath() {
+        String path = getClass().getResource("/").getPath();
+        return StringUtils.removeEnd(path, File.separator);
+    }
+
+    /**
+     * Get the username and password
+     *
+     * @return username:password
+     */
+    private String getUsernamePassword() {
+        return this.userName + ":" + this.password;
     }
 }
