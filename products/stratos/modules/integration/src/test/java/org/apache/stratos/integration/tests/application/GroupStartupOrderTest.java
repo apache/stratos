e59a6e1c791908101e330042e7f6c6d3790af974
reka <rthirunavukkarasu23@gmail.com>
2015-08-20 10:39:06 +0530

    fixing startup order test cases issues

--- products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/application/GroupStartupOrderTest.java
+++ products/stratos/modules/integration/src/test/java/org/apache/stratos/integration/tests/application/GroupStartupOrderTest.java
@@ -27,6 +27,7 @@ import org.apache.stratos.integration.tests.TopologyHandler;
 import org.apache.stratos.messaging.domain.application.ApplicationStatus;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +40,7 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
     private static final Log log = LogFactory.getLog(SampleApplicationsTest.class);
     private static final String RESOURCES_PATH = "/group-startup-order-test";
     private static final int GROUP_ACTIVE_TIMEOUT = 300000;
+    private static final int NODES_START_PARALLEL_TIMEOUT = 30000;
 
 
     @Test
@@ -82,11 +84,6 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
                     RestConstants.CARTRIDGES, RestConstants.CARTRIDGES_NAME);
             assertTrue(addedC8);
 
-            boolean addedG1 = restClient.addEntity(RESOURCES_PATH + RestConstants.CARTRIDGE_GROUPS_PATH +
-                            "/" + "esb-php-group-startup-order-test.json", RestConstants.CARTRIDGE_GROUPS,
-                    RestConstants.CARTRIDGE_GROUPS_NAME);
-            assertTrue(addedG1);
-
             boolean addedG2 = restClient.addEntity(RESOURCES_PATH + RestConstants.CARTRIDGE_GROUPS_PATH +
                             "/" + "group6-group-startup-order-test.json", RestConstants.CARTRIDGE_GROUPS,
                     RestConstants.CARTRIDGE_GROUPS_NAME);
@@ -128,26 +125,69 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
                     RestConstants.APPLICATIONS_NAME);
             assertTrue(deployed);
 
+            String group6 = topologyHandler.generateId(bean.getApplicationId(),
+                    "my-group6-group-startup-order-test", bean.getApplicationId() + "-1");
+
+            String group8 = topologyHandler.generateId(bean.getApplicationId(),
+                    "my-group8-group-startup-order-test", bean.getApplicationId() + "-1");
+
+            String lb = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-stratos-lb-group-startup-order-test");
+
+            String tomcat = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-tomcat-group-startup-order-test");
+
+            assertCreationOfNodes(lb, tomcat);
+
+            assertCreationOfNodes(tomcat, group6);
+
+            assertCreationOfNodesInParallel(group6, group8);
+
+            assertCreationOfNodes(tomcat, group8);
+
+            String group7 = topologyHandler.generateId(bean.getApplicationId(),
+                    "my-group7-group-startup-order-test", bean.getApplicationId() + "-1");
+
+            String groupTom2 = topologyHandler.generateId(bean.getApplicationId(),
+                    "my-group6-group-tom2-group-startup-order-test", bean.getApplicationId() + "-1");
+
+            assertCreationOfNodesInParallel(group7, groupTom2);
+
+            String group7Tomcat = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-group7-tomcat-group-startup-order-test");
+
+            String group7Tomcat1 = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-group7-tomcat1-group-startup-order-test");
+
+            assertCreationOfNodes(group7Tomcat, group7Tomcat1);
+
+            String groupTom2Tomcat2 = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-group-tom2-tomcat2-group-startup-order-test");
+
+            String groupTom2Tomcat3 = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-group-tom2-tomcat3-group-startup-order-test");
+
+            assertCreationOfNodes(groupTom2Tomcat2, groupTom2Tomcat3);
+
+            String group8Tomcat2 = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-tomcat2-group8-group-startup-order-test");
+
+            String group8Tomcat = topologyHandler.
+                    getClusterIdFromAlias(bean.getApplicationId(),
+                            "my-tomcat-group8-group-startup-order-test");
+
+            assertCreationOfNodesInParallel(group8Tomcat2, group8Tomcat);
+
             //Application active handling
             topologyHandler.assertApplicationStatus(bean.getApplicationId(),
                     ApplicationStatus.Active);
-            String groupId = topologyHandler.generateId(bean.getApplicationId(),
-                    "g-G1-1x0-group-startup-order-test", bean.getApplicationId() + "-1");
-
-            String clusterIdC3 = topologyHandler.
-                    getClusterIdFromAlias(bean.getApplicationId(),
-                            "c3-1x0-group-startup-order-test");
-
-            String clusterIdC4 = topologyHandler.
-                    getClusterIdFromAlias(bean.getApplicationId(),
-                            "c4-1x0-group-startup-order-test");
-
-            String clusterIdC2 = topologyHandler.
-                    getClusterIdFromAlias(bean.getApplicationId(),
-                            "c2-1x0-group-startup-order-test");
-
-            /*assertCreationOfNodes(groupId, clusterIdC2);
-            assertCreationOfNodes(clusterIdC3, clusterIdC4);
 
             //Group active handling
             topologyHandler.assertGroupActivation(bean.getApplicationId());
@@ -155,33 +195,13 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
             //Cluster active handling
             topologyHandler.assertClusterActivation(bean.getApplicationId());
 
-            List<String> clusterIds = new ArrayList<String>();
-            clusterIds.add(clusterIdC3);
-            clusterIds.add(clusterIdC4);
-            clusterIds.add(clusterIdC2);
-
-            assertGroupInactive(groupId, clusterIdC3);
-
-            assertTerminatingOfNodes(groupId, clusterIds);
-
-            assertTerminationOfNodes(groupId, clusterIds);
-
-            //Application active handling
-            topologyHandler.assertApplicationStatus(bean.getApplicationId(),
-                    ApplicationStatus.Active);
-
-            assertCreationOfNodes(groupId, clusterIdC2);
-
-            assertCreationOfNodes(clusterIdC3, clusterIdC4);
-
-            //Group active handling
-            topologyHandler.assertGroupActivation(bean.getApplicationId());
-
-            //Cluster active handling
-            topologyHandler.assertClusterActivation(bean.getApplicationId());*/
-
             boolean removedGroup = restClient.removeEntity(RestConstants.CARTRIDGE_GROUPS,
-                    "g-sc-G4-group-startup-order-test",
+                    "group6-group-startup-order-test",
+                    RestConstants.CARTRIDGE_GROUPS_NAME);
+            assertFalse(removedGroup);
+
+            removedGroup = restClient.removeEntity(RestConstants.CARTRIDGE_GROUPS,
+                    "group8-group-startup-order-test",
                     RestConstants.CARTRIDGE_GROUPS_NAME);
             assertFalse(removedGroup);
 
@@ -230,25 +250,34 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
             assertNull(beanRemoved);
 
             removedGroup = restClient.removeEntity(RestConstants.CARTRIDGE_GROUPS,
-                    "g-sc-G4-group-startup-order-test",
+                    "group6-group-startup-order-test",
                     RestConstants.CARTRIDGE_GROUPS_NAME);
             assertTrue(removedGroup);
 
-            boolean removedC1 = restClient.removeEntity(RestConstants.CARTRIDGES, "c1-group-startup-order-test",
+            removedGroup = restClient.removeEntity(RestConstants.CARTRIDGE_GROUPS,
+                    "group8-group-startup-order-test",
+                    RestConstants.CARTRIDGE_GROUPS_NAME);
+            assertTrue(removedGroup);
+
+            boolean removedC1 = restClient.removeEntity(RestConstants.CARTRIDGES, "stratos-lb-group-startup-order-test",
                     RestConstants.CARTRIDGES_NAME);
             assertTrue(removedC1);
 
-            boolean removedC2 = restClient.removeEntity(RestConstants.CARTRIDGES, "c2-group-startup-order-test",
+            boolean removedC2 = restClient.removeEntity(RestConstants.CARTRIDGES, "tomcat-group-startup-order-test",
                     RestConstants.CARTRIDGES_NAME);
             assertTrue(removedC2);
 
-            boolean removedC3 = restClient.removeEntity(RestConstants.CARTRIDGES, "c3-group-startup-order-test",
+            boolean removedC3 = restClient.removeEntity(RestConstants.CARTRIDGES, "tomcat1-group-startup-order-test",
                     RestConstants.CARTRIDGES_NAME);
             assertTrue(removedC3);
 
-            boolean removedC4 = restClient.removeEntity(RestConstants.CARTRIDGES, "c4-group-startup-order-test",
+            boolean removedC4 = restClient.removeEntity(RestConstants.CARTRIDGES, "tomcat2-group-startup-order-test",
                     RestConstants.CARTRIDGES_NAME);
             assertTrue(removedC4);
+
+            boolean removedC5 = restClient.removeEntity(RestConstants.CARTRIDGES, "tomcat3-group-startup-order-test",
+                    RestConstants.CARTRIDGES_NAME);
+            assertTrue(removedC5);
 
             removedAuto = restClient.removeEntity(RestConstants.AUTOSCALING_POLICIES,
                     autoscalingPolicyId, RestConstants.AUTOSCALING_POLICIES_NAME);
@@ -278,108 +307,58 @@ public class GroupStartupOrderTest extends StratosTestServerManager {
         }
     }
 
-    private void assertGroupInactive(String groupId, String clusterId) {
-        long startTime = System.currentTimeMillis();
-        Map<String, Long> inActiveMap = TopologyHandler.getInstance().getInActiveMembers();
-
-        while (!inActiveMap.containsKey(clusterId)) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignore) {
-            }
-            inActiveMap = TopologyHandler.getInstance().getInActiveMembers();
-            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                break;
-            }
-        }
-        assertTrue(inActiveMap.containsKey(clusterId));
-
-        while (!inActiveMap.containsKey(groupId)) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignore) {
-            }
-            inActiveMap = TopologyHandler.getInstance().getInActiveMembers();
-            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                break;
-            }
-        }
-        assertTrue(inActiveMap.containsKey(groupId));
-
-    }
-
-    private void assertTerminatingOfNodes(String groupId, List<String> clusterIds) {
-        Map<String, Long> terminatingMembers = TopologyHandler.getInstance().getTerminatingMembers();
-        for (String clusterId : clusterIds) {
-            long startTime = System.currentTimeMillis();
-            while (!terminatingMembers.containsKey(clusterId)) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ignore) {
-                }
-                terminatingMembers = TopologyHandler.getInstance().getTerminatingMembers();
-                if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                    break;
-                }
-            }
-            assertTrue(terminatingMembers.containsKey(groupId));
-        }
-        long startTime = System.currentTimeMillis();
-        while (!terminatingMembers.containsKey(groupId)) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignore) {
-            }
-            terminatingMembers = TopologyHandler.getInstance().getTerminatingMembers();
-            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                break;
-            }
-        }
-        assertTrue(terminatingMembers.containsKey(groupId));
-
-    }
-
-    private void assertTerminationOfNodes(String groupId, List<String> clusterIds) {
-        long startTime = System.currentTimeMillis();
-        Map<String, Long> terminatedMembers = TopologyHandler.getInstance().getTerminatedMembers();
-
-        for (String clusterId : clusterIds) {
-            while (!terminatedMembers.containsKey(clusterId)) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ignore) {
-                }
-                terminatedMembers = TopologyHandler.getInstance().getTerminatedMembers();
-                if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                    break;
-                }
-            }
-            assertTrue(terminatedMembers.containsKey(clusterId));
-        }
-
-        while (!terminatedMembers.containsKey(groupId)) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignore) {
-            }
-            terminatedMembers = TopologyHandler.getInstance().getTerminatedMembers();
-            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
-                break;
-            }
-        }
-
-        assertTrue(terminatedMembers.containsKey(groupId));
-    }
-
     private void assertCreationOfNodes(String firstNodeId, String secondNodeId) {
         //group1 started first, then cluster started later
-
+        long startTime = System.currentTimeMillis();
         Map<String, Long> activeMembers = TopologyHandler.getInstance().getActivateddMembers();
         Map<String, Long> createdMembers = TopologyHandler.getInstance().getCreatedMembers();
         //Active member should be available at the time cluster is started to create.
+        while(!activeMembers.containsKey(firstNodeId)) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            activeMembers = TopologyHandler.getInstance().getActivateddMembers();
+            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
+                break;
+            }
+        }
         assertTrue(activeMembers.containsKey(firstNodeId));
+
+        while(!createdMembers.containsKey(secondNodeId)) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            createdMembers = TopologyHandler.getInstance().getCreatedMembers();
+            if ((System.currentTimeMillis() - startTime) > GROUP_ACTIVE_TIMEOUT) {
+                break;
+            }
+        }
+
         assertTrue(createdMembers.containsKey(secondNodeId));
 
         assertTrue(createdMembers.get(secondNodeId) > activeMembers.get(firstNodeId));
+    }
+
+    private void assertCreationOfNodesInParallel(String firstNodeId, String secondNodeId) {
+        //group1 started first, then cluster started later
+        long startTime = System.currentTimeMillis();
+        Map<String, Long> createdMembers = TopologyHandler.getInstance().getCreatedMembers();
+        //Active member should be available at the time cluster is started to create.
+
+        while(!(createdMembers.containsKey(firstNodeId) && createdMembers.containsKey(firstNodeId))) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            createdMembers = TopologyHandler.getInstance().getCreatedMembers();
+            if ((System.currentTimeMillis() - startTime) > NODES_START_PARALLEL_TIMEOUT) {
+                break;
+            }
+        }
+        assertTrue(createdMembers.containsKey(firstNodeId));
+        assertTrue(createdMembers.containsKey(firstNodeId));
+
     }
 }
