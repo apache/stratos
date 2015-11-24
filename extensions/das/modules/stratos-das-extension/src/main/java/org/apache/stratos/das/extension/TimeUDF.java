41761afb230ecc7647dd037a53239a26afcb53ed
Thanuja <thanujau@wso2.com>
2015-11-24 12:24:34 +0530

    Restructuring das extension folder structure

--- extensions/das/modules/stratos-das-extension/src/main/java/org/apache/stratos/das/extension/TimeUDF.java
+++ extensions/das/modules/stratos-das-extension/src/main/java/org/apache/stratos/das/extension/TimeUDF.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.stratos.das.extension.spark.udf;
+package org.apache.stratos.das.extension;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
